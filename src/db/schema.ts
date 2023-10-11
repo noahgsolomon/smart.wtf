fb4aaffb8b719a3b33e724e5693576e246132e57
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-11 17:32:40 -0400

    shadcn

---
+++ src/db/schema.ts
@@ -0,0 +1,25 @@
+import { mysqlTable, mysqlSchema, AnyMySqlColumn, primaryKey, unique, int, varchar } from "drizzle-orm/mysql-core"
+import { sql } from "drizzle-orm"
+
+
+export const authors = mysqlTable("authors", {
+	id: int("id").autoincrement().notNull(),
+	author: varchar("author", { length: 255 }).notNull(),
+},
+(table) => {
+	return {
+		authorsId: primaryKey(table.id),
+		author: unique("author").on(table.author),
+	}
+});
+
+export const categories = mysqlTable("categories", {
+	id: int("id").autoincrement().notNull(),
+	category: varchar("category", { length: 255 }).notNull(),
+},
+(table) => {
+	return {
+		categoriesId: primaryKey(table.id),
+		category: unique("category").on(table.category),
+	}
+});
