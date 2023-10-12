00f727ed7a3b1dd307f8160826815ce33512e5fb
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-12 14:48:16 -0400

    log in with clerk

---
+++ src/app/login/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs'
+
+const LoginPage = () => {
+
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <SignIn
+      appearance={{
+        elements: {
+          alertText: 'text-primary',  
+          card: ' rounded-lg border border-border bg-card',
+          headerTitle: 'text-primary',
+          headerSubtitle: 'text-primary opacity-60',
+          socialButtonsBlockButton: 'rounded-lg py-3 border border-secondary hover:bg-secondary',
+          socialButtonsBlockButtonText: 'text-secondary-foreground',
+          footerActionText: 'text-primary opacity-40',
+          footerActionLink: 'transition-all',
+        }
+      }}
+      />
+    </div>
+    
+  )
+}
+
+export default LoginPage;
