81ae3f5b19a684f8208f6e81ecbc432f12e932a1
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-12 14:46:54 -0400

    page with routing to sign up and login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 import { Button } from "@/components/ui/button";
 import ThemeButton from "@/components/ui/theme";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  
+  const {userId} = auth();
+
+  console.log(userId);
+
+  if (userId) {
+    redirect('/dashboard');
+  }
 
   return (
-    <div className="gap-2 first-letter:h-screen w-screen flex justify-center items-center">
-        <ThemeButton />
-        <Link href={'signup'}><Button>Sign up</Button></Link>
-      </div>
+    <div>
+      <header className='p-5'>
+                <div className="gap-4 flex justify-end items-center">
+                    <ThemeButton />
+                    <Link href={'signup'}><Button>Sign up</Button></Link>
+                    <Link href={'login'}><Button>Log in</Button></Link>
+                </div>
+      </header>
+      <main className="flex justify-center items-center mt-20">
+          <div>Sup, stranger</div>
+      </main>
+    </div>
   )
 }
