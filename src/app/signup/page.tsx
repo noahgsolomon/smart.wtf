3ffa0253ee1727a8fcbfa17ad8b77d761e340972
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-12 14:48:04 -0400

    sign up with clerk

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import { SignUp } from '@clerk/nextjs'
 
+
 const SignUpPage = () => {
   return (
-    <SignUp />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <SignUp appearance={{
+        elements: {
+          alertText: 'text-primary',  
+          card: ' rounded-lg border border-border bg-card',
+          headerTitle: 'text-primary',
+          headerSubtitle: 'text-primary opacity-60',
+          socialButtonsBlockButton: 'rounded-lg py-3 border border-secondary hover:bg-secondary',
+          socialButtonsBlockButtonText: 'text-secondary-foreground',
+          footerActionText: 'text-primary opacity-40',
+          footerActionLink: 'transition-all',
+        }
+      }}/>
+    </div>
+    
   )
 }
 
