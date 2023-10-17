b0303745cfef0c816fb9e3ab9820fce67f9e513c
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-17 16:47:22 -0400

    simpler trpc setup with protectedProcedure. No need for initial context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,17 +1,15 @@
-
-import { appRouter } from '@/app/server';
-import { createContext, createContextInner } from '@/app/server/trpc';
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { NextRequest } from 'next/server';
+import { appRouter } from "@/app/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { NextRequest } from "next/server";
 
 const handler = async (req: NextRequest) => {
-    const response = await fetchRequestHandler({
-        endpoint: '/api/trpc',
-        req,
-        router: appRouter,
-        createContext: () => (createContext(req)),
-    });
-    return response;
-}
+	const response = await fetchRequestHandler({
+		endpoint: "/api/trpc",
+		req,
+		router: appRouter,
+		createContext: () => ({}),
+	});
+	return response;
+};
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
