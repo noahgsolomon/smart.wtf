a007480dd8e0ebc119ac938c97fbc7086fc9651a
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-17 16:46:32 -0400

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+import { buttonVariants } from '@/components/ui/button';
+import { currentUser, useUser } from '@clerk/nextjs';
+import Link from 'next/link';
 
-export default function Home() {
+const Home = async () => {
+	const user = await currentUser();
 
-  return (
-    <div>
-      <main className="flex justify-center items-center mt-20">
-          <div>Sup, stranger</div>
-      </main>
-    </div>
-  )
-}
+	return (
+		<div>
+			<main className="flex gap-4 flex-col justify-center items-center mt-20">
+				{user ? (
+					<>
+						<div>Sup, {user.firstName}</div>
+						<Link
+							href="/dashboard"
+							className={buttonVariants({ variant: 'outline' })}
+						>
+							Dashboard
+						</Link>
+					</>
+				) : (
+					<div>Sup</div>
+				)}
+			</main>
+		</div>
+	);
+};
+
+export default Home;
