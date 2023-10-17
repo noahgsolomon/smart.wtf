2ad9f97012e3d79c0021a1a414d8a1f8c6a8de12
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-17 16:48:25 -0400

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,19 @@
-const Dashboard = () => {
+import UserDashboard from '@/components/userdashboard';
+import { Loader2 } from 'lucide-react';
+import { Suspense } from 'react';
 
-    return (
-        <>
-        <main>
-            <div className="flex justify-center">
-                
-                {/* {await serverClient.hey()} */}
-            </div>
-        </main>
-        </>
-        
-    );
-}
+const Dashboard = () => {
+	return (
+		<>
+			<main>
+				<div className="flex justify-center mt-20">
+					<Suspense fallback={<Loader2 className="w-4 h-4 animate-spin" />}>
+						<UserDashboard />
+					</Suspense>
+				</div>
+			</main>
+		</>
+	);
+};
 
 export default Dashboard;
