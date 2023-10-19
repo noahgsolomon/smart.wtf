caa06076bbde601e76fc32960c21ebb799bc3059
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-19 16:45:01 -0400

    theme initial loader

--- src/components/ui/theme.tsx
+++ src/components/ui/theme.tsx
@@ -1,31 +1,39 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "lucide-react";
+import { Loader2, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { Button } from "./button";
 
 export default function ThemeButton() {
-	const { resolvedTheme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
-	const [mounted, setMounted] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
-	useEffect(() => setMounted(true), []);
+  useEffect(() => setMounted(true), []);
 
-	if (!mounted) return null;
+  if (!mounted)
+    return (
+      <Button
+        variant={"ghost"}
+        disabled={true}
+        className="opacity-80 transition-all"
+      >
+        <Loader2 className="h-4 w-4 animate-spin" />
+      </Button>
+    );
 
-	return (
-		<Button
-			variant={"ghost"}
-			className={`${!mounted ? "opacity-80" : ""}`}
-			disabled={!mounted}
-			onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-		>
-			{resolvedTheme === "light" ? (
-				<SunIcon className="w-4 h-4" />
-			) : (
-				<MoonIcon className="w-4 h-4" />
-			)}
-		</Button>
-	);
+  return (
+    <Button
+      variant={"ghost"}
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      className="transition-all"
+    >
+      {resolvedTheme === "light" ? (
+        <SunIcon className="h-4 w-4" />
+      ) : (
+        <MoonIcon className="h-4 w-4" />
+      )}
+    </Button>
+  );
 }
