4621fe415a2245b5d962901bf575133f74f67e25
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-19 07:41:21 -0400

    navbar changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,15 @@
-import { FunctionComponent } from "react";
+import { Suspense, type FunctionComponent } from "react";
 import ThemeButton from "./ui/theme";
 import { buttonVariants } from "./ui/button";
-import { UserButton, auth } from "@clerk/nextjs";
+import { SignInButton, UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import { Cloud } from "lucide-react";
 
-interface NavBarProps {}
-
-const NavBar: FunctionComponent<NavBarProps> = () => {
+const NavBar: FunctionComponent = () => {
   const { userId } = auth();
 
   return (
-    <header className="r border-sm fixed left-0 right-0 top-0 border-b border-border p-3 backdrop-blur-sm">
+    <header className="border-sm fixed left-0 right-0 top-0 z-10 border-b border-border p-3 backdrop-blur-sm">
       <div className="flex justify-between">
         <div>
           <Link className={buttonVariants({ variant: "ghost" })} href={"/"}>
@@ -21,24 +19,34 @@ const NavBar: FunctionComponent<NavBarProps> = () => {
         <div className="flex items-center justify-end gap-4">
           <ThemeButton />
           {userId ? (
-            <UserButton
-              afterSignOutUrl="/"
-              appearance={{
-                elements: {
-                  rootBox: "w-10 h-10 flex items-center",
-                  avatarBox:
-                    "border-black border-[1px] dark:border-white hover:opacity-80 transition-all",
-                  userButtonPopoverCard:
-                    "rounded-lg border border-border bg-card",
-                  userPreviewMainIdentifier: "text-primary",
-                  userPreviewSecondaryIdentifier: "text-primary opacity-60",
-                  userButtonPopoverFooter: "opacity-0 hidden",
-                  userButtonPopoverActionButton: "hover:bg-primary-foreground",
-                  userButtonPopoverActionButtonText: "text-primary",
-                  userButtonPopoverActionButtonIcon: "text-primary",
-                },
-              }}
-            />
+            <>
+              <Link
+                href={"/dashboard"}
+                className={buttonVariants({ variant: "outline" })}
+              >
+                Dashboard
+              </Link>
+              <UserButton
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    userButtonTrigger: "focus:outline-none focus:ring-0 ",
+                    rootBox: "w-10 h-10 flex items-center ",
+                    avatarBox:
+                      "border-border rounded-lg border-[1px] hover:opacity-80 transition-all w-9 h-9 active:border-2",
+                    userButtonPopoverCard:
+                      "rounded-lg border border-border bg-card",
+                    userPreviewMainIdentifier: "text-primary",
+                    userPreviewSecondaryIdentifier: "text-primary opacity-60",
+                    userButtonPopoverFooter: "opacity-0 hidden",
+                    userButtonPopoverActionButton:
+                      "hover:bg-primary-foreground",
+                    userButtonPopoverActionButtonText: "text-primary",
+                    userButtonPopoverActionButtonIcon: "text-primary",
+                  },
+                }}
+              />
+            </>
           ) : (
             <>
               <Link
