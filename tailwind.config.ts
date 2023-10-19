0731b6f76b9068945a944425967fab5c22fe5ef5
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-19 07:37:14 -0400

    new shadow and link color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,11 +50,23 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        link: {
+          DEFAULT: "hsl(var(--link))",
+          hover: "hsl(var(--link-hover))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        sm: "0 0 5px rgba(0, 0, 0, 0.1)",
+        DEFAULT: "0 0 10px rgba(0, 0, 0, 0.1)",
+        md: "0 0 15px rgba(0, 0, 0, 0.1)",
+        lg: "0 0 25px rgba(0, 0, 0, 0.1)",
+        xl: "0 0 35px rgba(0, 0, 0, 0.1)",
+        "2xl": "0 0 50px rgba(0, 0, 0, 0.1)",
       },
       keyframes: {
         "accordion-down": {
