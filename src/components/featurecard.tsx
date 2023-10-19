707afdc8daf5daac776a8e9b2bcfa726ad7cadd4
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-19 07:41:00 -0400

    feature card

---
+++ src/components/featurecard.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import React, { FC, useEffect, useState } from "react";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  type MotionStyle,
+  type MotionValue,
+} from "framer-motion";
+import { cn } from "@/lib/utils";
+
+type WrapperStyle = MotionStyle & {
+  "--x": MotionValue<string>;
+  "--y": MotionValue<string>;
+};
+const FeatureCard: FC<{
+  children: React.ReactNode;
+  className?: string;
+}> = ({ children, className }) => {
+  const [, setMounted] = useState(false);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  function handleMouseMove({
+    currentTarget,
+    clientX,
+    clientY,
+  }: React.MouseEvent<HTMLDivElement>) {
+    if (!currentTarget) return;
+    const { left, top } = currentTarget.getBoundingClientRect();
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <motion.div
+      className={cn(
+        "animated-feature-cards group relative overflow-hidden drop-shadow-[0_0_15px_rgba(49,49,49,0.2)] dark:drop-shadow-[0_0_15px_rgba(49,49,49,0.2)]",
+        className,
+      )}
+      onMouseMove={handleMouseMove}
+      style={
+        {
+          "--x": useMotionTemplate`${mouseX}px`,
+          "--y": useMotionTemplate`${mouseY}px`,
+        } as WrapperStyle
+      }
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default FeatureCard;
