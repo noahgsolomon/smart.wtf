6f0e0763cc5a9d1d105b5d90a20135e52caf1b00
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-21 13:25:08 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,56 +8,11 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { headers } from "next/headers";
 import ProgressBarProvider from "./progressbar";
 import { Analytics } from "@vercel/analytics/react";
-import { siteConfig } from "@/config/site";
+import { constructMetadata } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    "Learning platform",
-    "Interactive learning",
-    "System design",
-    "AI and machine learning",
-    "Computer science",
-  ],
-  authors: [
-    {
-      name: "noahgsolomon",
-      url: "https://twitter.com/noahgsolomon",
-    },
-  ],
-  creator: "noahgsolomon",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`https://images.codefoli.com/smartwtf.png`],
-    creator: "@noahgsolomon",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-};
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
