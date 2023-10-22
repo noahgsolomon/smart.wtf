cc975e169d620917aa1036ceee6910654a76f141
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-22 15:39:14 -0400

    achievement placeholder

---
+++ src/app/settings/achievements/page.tsx
@@ -0,0 +1,15 @@
+import SideBar from "../components/sidebar";
+
+const Page = () => {
+  return (
+    <div className="mx-10 mt-28 flex w-full flex-col">
+      <h3 className="mb-2 text-3xl font-bold">Achievements</h3>
+      <p className="mb-4 opacity-60 ">View account achievements</p>
+      <div className="rounded-lg border border-border p-4">
+        Not yet implemented...
+      </div>
+    </div>
+  );
+};
+
+export default Page;
