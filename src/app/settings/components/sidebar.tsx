7c198bc0d189e9c76f085bd11c4082494482b2dd
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-22 15:38:30 -0400

    sidebar

---
+++ src/app/settings/components/sidebar.tsx
@@ -0,0 +1,43 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Fish, Landmark, Settings } from "lucide-react";
+import Link from "next/link";
+
+const SideBar = () => {
+  return (
+    <div className="ml-10 mt-20 hidden flex-col items-center justify-start md:flex">
+      <Link
+        href="/settings/achievements"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "pr-30 mx-1 my-1 flex w-full flex-row justify-start gap-2 rounded-lg pl-4 transition-all hover:bg-secondary",
+        )}
+      >
+        <Fish className="h-4 w-4" />
+        Achievements
+      </Link>
+      <Link
+        href="/settings/billing"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "pr-30 mx-1 my-1 flex w-full flex-row justify-start gap-2 rounded-lg pl-4 transition-all hover:bg-secondary",
+        )}
+      >
+        <Landmark className="h-4 w-4" />
+        Billing
+      </Link>
+      <Link
+        href="/settings"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "pr-30 mx-1 my-1 flex w-full flex-row justify-start gap-2 rounded-lg pl-4 transition-all hover:bg-secondary",
+        )}
+      >
+        <Settings className="h-4 w-4" />
+        Settings
+      </Link>
+    </div>
+  );
+};
+
+export default SideBar;
