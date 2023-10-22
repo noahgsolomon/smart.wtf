cbee0a5fdab8a26c9d70e0ecaa1fd831d2b15729
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-22 15:39:20 -0400

    settings placeholder

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div className="mx-10 mt-28 flex w-full flex-col">
+      <h3 className="mb-2 text-3xl font-bold">Settings</h3>
+      <p className="mb-4 opacity-60 ">Manage account settings</p>
+    </div>
+  );
+};
+
+export default Page;
