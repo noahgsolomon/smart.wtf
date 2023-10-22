22092826f18c5c70b90ad91fa2fc872f87b95e49
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-22 15:38:57 -0400

    dashboard placeholder

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,9 @@
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
-
 const Dashboard = () => {
   return (
     <>
-      <main className="mt-20">
-        <div className="flex items-center justify-center">
-          <div className="rounded-lg border-border">some shit</div>
+      <main className="mt-20 h-screen">
+        <div className="flex h-full items-center justify-center">
+          <div className="rounded-lg border-border">some dashboard shit</div>
         </div>
       </main>
     </>
