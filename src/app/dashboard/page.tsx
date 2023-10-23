d562c0ef5a49638000ef96ee4ffdb6a6d8160811
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-23 15:47:28 -0400

    start to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,20 @@
-const Dashboard = () => {
+import { api } from "@/trpc/server";
+
+const Dashboard = async () => {
+  const user = (await api.user.user.query()).user;
+
   return (
     <>
-      <main className="mt-20 h-screen">
-        <div className="flex h-full items-center justify-center">
-          <div className="rounded-lg border-border">some dashboard shit</div>
+      <main className="mx-20 mt-48 h-[calc(100vh-20rem)]">
+        <div className="">
+          <div>
+            <h3 className="text-4xl">Welcome back, {user?.name}</h3>
+          </div>
+          {/* <div>
+            <div className="h-[300px] w-[500px] rounded-lg border border-border shadow-lg shadow-secondary transition-all lg:w-[600px]">
+              <div>built by buildstreak</div>
+            </div>
+          </div> */}
         </div>
       </main>
     </>
