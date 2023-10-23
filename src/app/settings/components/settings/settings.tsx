8ceb432d02e9888db605030ea03026d1e1c906a8
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-23 10:21:38 -0400

    settings page

---
+++ src/app/settings/components/settings/settings.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import AvatarUpload from "./avatarupload";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import SettingsInput from "./settingsinput";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useClerk } from "@clerk/nextjs";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+import { trpc } from "@/trpc/client";
+
+const Settings = () => {
+  const { toast } = useToast();
+  const clerk = useClerk();
+
+  const [deleting, setDeleting] = useState(false);
+
+  const logOutHandler = async () => {
+    await clerk.signOut();
+  };
+
+  const deleteUser = trpc.user.delete.useMutation();
+
+  const deleteHandler = () => {
+    setDeleting(true);
+    try {
+      deleteUser
+        .mutateAsync(undefined, {
+          onError: (data) => {
+            toast({
+              title: "Error",
+              description: data.message,
+              variant: "destructive",
+            });
+          },
+        })
+        .catch.bind(deleteUser.mutateAsync);
+      clerk.user?.delete().catch.bind(clerk.user?.delete);
+      logOutHandler().catch.bind(logOutHandler);
+    } catch (e) {
+      toast({
+        title: "Error",
+        description: "error message",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <div className="rounded-lg border border-border p-4">
+      <AvatarUpload />
+      <SettingsInput />
+      <div className="mt-4 flex">
+        <div className="flex flex-col">
+          <Button
+            className="mt-4 shadow-none"
+            variant={"outline"}
+            onClick={logOutHandler}
+          >
+            log out
+          </Button>
+          <Dialog>
+            <DialogTrigger
+              className={buttonVariants({
+                variant: "destructive",
+                className: "mt-4 shadow-none",
+              })}
+            >
+              delete account
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter className="justify-start gap-2">
+                <DialogClose asChild>
+                  <Button
+                    className="shadow-none"
+                    type="button"
+                    variant="secondary"
+                  >
+                    Close
+                  </Button>
+                </DialogClose>
+                <Button
+                  onClick={deleteHandler}
+                  type="submit"
+                  disabled={deleting}
+                  className="shadow-none"
+                  variant={"destructive"}
+                >
+                  {deleting ? (
+                    <Loader2 className="h-4 w-4 animate-spin" />
+                  ) : (
+                    "Delete"
+                  )}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default Settings;
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,8 +1,11 @@
+import Settings from "./components/settings/settings";
+
 const Page = () => {
   return (
     <div className="mx-10 mt-40 flex w-full flex-col">
       <h3 className="mb-2 text-3xl font-bold">Settings</h3>
       <p className="mb-4 opacity-60 ">Manage account settings</p>
+      <Settings />
     </div>
   );
 };
