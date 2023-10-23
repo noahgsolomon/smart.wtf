5c6a2f9ff186312caa934b9e03620377ba508511
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-23 10:21:26 -0400

    change username or name

---
+++ src/app/settings/components/settings/settingsinput.tsx
@@ -0,0 +1,132 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { trpc } from "@/trpc/client";
+import { useState } from "react";
+
+const SettingsInput = () => {
+  const userDB = trpc.user.user.useQuery();
+  const { toast } = useToast();
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+
+  const nameUpload = trpc.user.setName.useMutation();
+  const usernameUpload = trpc.user.setUsername.useMutation();
+
+  const handleUsernameSubmit = () => {
+    if (username.length < 3 || username.length > 20) {
+      toast({
+        title: "Error",
+        description: "Username must be between 3 and 20 characters",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    usernameUpload.mutate(
+      { username },
+      {
+        onSuccess: (data) => {
+          toast({
+            title: data.status === "OK" ? "Sucess!" : "Error",
+            description: data.message,
+            variant: data.status === "OK" ? "success" : "destructive",
+          });
+          setUsername("");
+          if (data.status === "OK") {
+            userDB.refetch().catch.bind(userDB.refetch);
+          }
+        },
+      },
+    );
+  };
+
+  const handleNameSubmit = () => {
+    if (name.length < 3 || name.length > 75) {
+      toast({
+        title: "Error",
+        description: "Name must be between 3 and 75 characters",
+        variant: "destructive",
+      });
+      return;
+    }
+    nameUpload.mutate(
+      { name },
+      {
+        onSuccess: (data) => {
+          toast({
+            title: data.status === "OK" ? "Sucess!" : "Error",
+            description: data.message,
+            variant: data.status === "OK" ? "success" : "destructive",
+          });
+          setName("");
+          if (data.status === "OK") {
+            userDB.refetch().catch.bind(userDB.refetch);
+          }
+        },
+      },
+    );
+  };
+
+  return (
+    <div className="mt-4 flex w-full items-center gap-1.5">
+      <div className="space-y-4">
+        <div>
+          <Label htmlFor="username">username</Label>
+          <div className="flex flex-row gap-2">
+            <Input
+              className="shadow-none"
+              type="username"
+              id="username"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  handleUsernameSubmit();
+                }
+              }}
+              placeholder={"@" + userDB.data?.user?.username}
+            />
+            <Button
+              className={"shadow-none"}
+              variant={"secondary"}
+              onClick={handleUsernameSubmit}
+            >
+              save
+            </Button>
+          </div>
+        </div>
+        <div>
+          <Label htmlFor="name">name</Label>
+          <div className="flex flex-row gap-2">
+            <Input
+              className="shadow-none"
+              type="name"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  handleNameSubmit();
+                }
+              }}
+              placeholder={userDB.data?.user?.name}
+            />
+            <Button
+              className={"shadow-none"}
+              variant={"secondary"}
+              onClick={handleNameSubmit}
+            >
+              save
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SettingsInput;
