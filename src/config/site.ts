d0344db8a2342323c17f255964b058dd79e272b9
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-24 19:12:38 -0400

    new card

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "smart wtf",
   description: "Learning platform for the next generation of developers.",
   url: "https://smart.wtf",
-  ogImage: "https://images.codefoli.com/smartwtf.png",
+  ogImage: "https://images.codefoli.com/smartwtfunderwater.png",
   links: {
     twitter: "https://twitter.com/noahgsolomon",
     github: "https://github.com/noahgsolomon",
