4be97d564da61591a0c7c9a31c15218ee63f3adc
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-25 17:04:35 -0400

    chat route

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+import Chat from "../dashboard/components/chat";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center px-5">
+      <Chat />
+    </div>
+  );
+};
+
+export default Page;
