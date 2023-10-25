2af3ba12da1d6a519851b4781de4e124d94a0835
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-24 22:31:17 -0400

    current dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { api } from "@/trpc/server";
-import CourseCard from "./components/coursecard";
 import Streak from "./components/streak";
-import blazing from "public/blazing.png";
 import cool from "public/cool.png";
 import Image from "next/image";
+import LatestAchievements from "./components/latestachievements";
+import SystemsDesignCourseCard from "./components/systemsdesigncoursecard";
+import WebOptimizationCourseCard from "./components/weboptimizationcoursecard";
 
 const Dashboard = async () => {
   const user = (await api.user.user.query()).user;
@@ -18,8 +19,9 @@ const Dashboard = async () => {
                 <h3 className="text-4xl">Welcome back, {user?.name}</h3>
                 <Image width={50} height={50} src={cool} alt="wave emoji" />
               </div>
-              <div>
+              <div className="flex flex-col gap-2">
                 <Streak />
+                <LatestAchievements />
               </div>
             </div>
           </section>
@@ -27,8 +29,9 @@ const Dashboard = async () => {
         <section className="py-10">
           <div className="rounded-lg border border-border p-8">
             <h1>Courses</h1>
-            <div className="flex flex-wrap py-4 ">
-              <CourseCard />
+            <div className="flex flex-wrap gap-8 py-4">
+              <SystemsDesignCourseCard />
+              <WebOptimizationCourseCard />
             </div>
           </div>
         </section>
