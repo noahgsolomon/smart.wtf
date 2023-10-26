dcf3641c58876adfdf33fb19dc11cc2ee9897e3d
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-26 17:28:44 -0400

    larger chat in chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,7 +3,7 @@ import Chat from "../dashboard/components/chat";
 const Page = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center px-5">
-      <Chat />
+      <Chat page={true} className="max-w-[800px]" />
     </div>
   );
 };
