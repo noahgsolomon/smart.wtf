a70f3f49c5579a95ca2d2437a30a3e7c184bb0d3
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-28 17:15:51 -0400

    get course by slug route

--- src/server/api/routers/courses.ts
+++ src/server/api/routers/courses.ts
@@ -1,46 +1,72 @@
-import { courseLikes } from "@/server/db/schemas/courses/schema";
+import { courseLikes, courses } from "@/server/db/schemas/courses/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 
-
 export const courseRouter = createTRPCRouter({
-    getCourses: protectedProcedure.query(async ({ctx}) => {
-        const courses = await ctx.db.query.courses.findMany({
-            with: {
-                instructor: {
-                    columns: {
-                        name: true,
-                    }
-                },
-                courseLikes: {
-                    where: eq(courseLikes.userId, ctx.user_id),
-                    columns: {
-                        courseId: true
-                    }
-                }
-            }
-        });
+  getCourses: protectedProcedure.query(async ({ ctx }) => {
+    const courses = await ctx.db.query.courses.findMany({
+      with: {
+        instructor: {
+          columns: {
+            name: true,
+          },
+        },
+        courseLikes: {
+          where: eq(courseLikes.userId, ctx.user_id),
+          columns: {
+            courseId: true,
+          },
+        },
+      },
+    });
 
-        // for each course, check whether the current user has liked it or not. this is in the course_likes table, 
-        // and it exists if the there is a row with the course_id and the user_id, and join this in the courses return
+    // for each course, check whether the current user has liked it or not. this is in the course_likes table,
+    // and it exists if the there is a row with the course_id and the user_id, and join this in the courses return
 
-        return {courses: courses};
+    return { courses: courses };
+  }),
+
+  getCourseBySlug: protectedProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const course = await ctx.db.query.courses.findFirst({
+        where: eq(courses.slug, input.slug),
+        with: {
+          instructor: {
+            columns: {
+              name: true,
+            },
+          },
+          courseLikes: {
+            where: eq(courseLikes.userId, ctx.user_id),
+            columns: {
+              courseId: true,
+            },
+          },
+        },
+      });
+
+      return { course: course };
     }),
 
-    addLike: protectedProcedure.input(z.object({courseId: z.number()})).mutation(async ({input, ctx}) => {
-        const like = await ctx.db.query.courseLikes.findFirst({
-            where: (and(eq(courseLikes.courseId, input.courseId), eq(courseLikes.userId, ctx.user_id)))
-        });
+  addLike: protectedProcedure
+    .input(z.object({ courseId: z.number() }))
+    .mutation(async ({ input, ctx }) => {
+      const like = await ctx.db.query.courseLikes.findFirst({
+        where: and(
+          eq(courseLikes.courseId, input.courseId),
+          eq(courseLikes.userId, ctx.user_id),
+        ),
+      });
 
-        if (like) {
-            await ctx.db.delete(courseLikes).where(eq(courseLikes.id, like.id));
-        }
-        else {
-            await ctx.db.insert(courseLikes).values({
-                courseId: input.courseId,
-                userId: ctx.user_id
-            });
-        }
+      if (like) {
+        await ctx.db.delete(courseLikes).where(eq(courseLikes.id, like.id));
+      } else {
+        await ctx.db.insert(courseLikes).values({
+          courseId: input.courseId,
+          userId: ctx.user_id,
+        });
+      }
     }),
 });
