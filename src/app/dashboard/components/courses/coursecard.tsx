2b14553c1f0a692ac71f1bc0f25347df1393b156
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-27 20:33:43 -0400

    course card

---
+++ src/app/dashboard/components/courses/coursecard.tsx
@@ -0,0 +1,98 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
+import { Circle } from "lucide-react";
+import Image from "next/image";
+import wtfCoin from "public/wtfcoin.png";
+import CourseLikeButton from "./likebutton";
+
+
+const CourseCard = ({course}: 
+        {course: {
+    id: number;
+    name: string;
+    description: string;
+    instructorId: number;
+    likesCount: number;
+    imageUrl: string;
+    difficulty: "EASY" | "MEDIUM" | "HARD";
+    instructor: {
+        name: string;
+    };
+    courseLikes: {
+        courseId?: number;
+    }[]
+        }}
+        ) => {
+
+    return (
+        <div className="relative">
+      <Badge className="absolute -left-2 -top-2 z-10 rounded-lg">new</Badge>
+      <CourseLikeButton courseId={course.id} clicked={course.courseLikes.length > 0}/>
+      <div className="group cursor-pointer overflow-hidden rounded-lg border border-border bg-card shadow-sm transition-all hover:border-primary hover:shadow-md ">
+        <div className="relative overflow-hidden border-b border-border">
+          <div className="transition-all group-hover:scale-[105%]">
+            <Image
+              src={course.imageUrl}
+              width={300}
+              height={300}
+              className="rounded-t-lg"
+              alt="systems design group-hover:bg-black"
+            />
+          </div>
+          <div className="absolute bottom-2 left-2">
+            <Badge variant={course.difficulty === 'EASY' ? 'easy' : course.difficulty === 'MEDIUM' ? 'medium' : 'hard'}>{course.difficulty}</Badge>
+          </div>
+        </div>
+
+        <div className="flex min-h-[200px] flex-col justify-between gap-2 p-4">
+          <div className="flex flex-row justify-between">
+            <h2 className="text-xl">{course.name}</h2>
+            <div className="flex flex-row items-center gap-1 rounded-lg bg-secondary p-1 text-xs font-bold text-opacity-60">
+              <Circle className="fill-blue h-3 w-3 text-secondary" />
+              STARTED
+            </div>
+          </div>
+          <div className="max-w-[35ch] text-xs opacity-60">
+            {course.description}
+          </div>
+
+          <div className="flex flex-col">
+            <h3 className="text-sm">Progress</h3>
+            <div className="flex flex-row items-center gap-2">
+              <h3 className="text-xs opacity-60">10%</h3>
+              <Progress
+                className=" bg-secondary"
+                indicatorClassName="bg-blue rounded-r-lg"
+                value={10}
+              />
+            </div>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-row items-center gap-2">
+              <div>
+                <Avatar className="h-[35px] w-[35px] border border-border">
+                  <AvatarImage
+                    className={`object-cover transition-all`}
+                    src={"https://images.codefoli.com/ricknmort.png"}
+                  />
+                  <AvatarFallback>{course.instructor.name.slice(0).toUpperCase()}</AvatarFallback>
+                </Avatar>
+              </div>
+              <div>
+                <h3 className="text-sm">{course.instructor.name}</h3>
+                <h3 className="text-xs">Instructor</h3>
+              </div>
+            </div>
+            <div className="group flex flex-row items-center rounded-lg border border-border bg-background p-1 transition-all hover:scale-105">
+              <Image width={30} height={30} src={wtfCoin} alt="wtf coin" />
+              <p className="text-sm"> + 500</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default CourseCard;
