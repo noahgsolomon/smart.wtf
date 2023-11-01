2f12f3c4f583722d4d342275661fe03b04dad2ad
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-01 18:40:27 -0400

    lesson beginning

--- src/app/lesson/[slug]/[chapter]/[lesson]/page.tsx
+++ src/app/lesson/[slug]/[chapter]/[lesson]/page.tsx
@@ -1,7 +1,25 @@
-export default function Page() {
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export default async function Page() {
+  const file = await new Promise((resolve) => {
+    resolve(`# Welcome to my MDX page!
+
+  This is some **ss bold** and _italics_ text.
+
+  This is a list in markdown:
+
+  - One
+  - Two
+  - Three
+
+  Checkout my React component:`);
+  });
+
   return (
-    <div className="mt-20 flex justify-center ">
-      <div>Lesson</div>
+    <div className="mt-28 flex justify-center">
+      <div className=" prose dark:prose-invert">
+        {typeof file === "string" ? <MDXRemote source={file} /> : null}
+      </div>
     </div>
   );
 }
