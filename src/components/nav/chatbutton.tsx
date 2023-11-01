71cc87468af477ddc92a906d4fb6461c90337a8c
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-01 18:40:44 -0400

    chat button query and lesson props

--- src/components/nav/chatbutton.tsx
+++ src/components/nav/chatbutton.tsx
@@ -2,16 +2,33 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { MessageSquare } from "lucide-react";
 
-const ChatButton = () => {
+const ChatButton = ({
+  lesson = false,
+  query = "",
+}: {
+  lesson?: boolean;
+  query?: string;
+}) => {
   const pathname = usePathname();
+
+  console.log("QUEERRRY", query);
 
   return (
     <>
-      {pathname === "/chat" ? null : (
-        <Link href={"/chat"} className={buttonVariants({ variant: "outline" })}>
+      {pathname === `/lesson/chat?${query}` || pathname === `/chat?${query}` ? (
+        <Button variant={"default"}>
+          <MessageSquare className="h-4 w-4" />
+        </Button>
+      ) : (
+        <Link
+          href={lesson ? `/lesson/chat?${query}` : `/chat?${query}`}
+          className={buttonVariants({
+            variant: "default",
+          })}
+        >
           <MessageSquare className="h-4 w-4" />
         </Link>
       )}
