dda6404c1024b68ad4c79d5332ef22e5ab527691
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-01 18:41:21 -0400

    mdx and typography stuffs

--- next.config.mjs
+++ next.config.mjs
@@ -2,34 +2,27 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-
 /** @type {import("next").NextConfig} */
-const config = {
+
+import nextMDX from "@next/mdx";
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+const nextConfig = {
   experimental: {
     ppr: true,
+    mdxRs: true,
   },
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.icons8.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.codefoli.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    domains: ["img.icons8.con", "img.clerk.com", "images.codefoli.com"],
   },
 };
 
-export default config;
+export default withMDX(nextConfig);
