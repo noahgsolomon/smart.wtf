3744b74fdc3ff9c12b36b370fecf60f27486bdd9
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-01 18:40:54 -0400

    custom heading for lessons

--- src/components/nav/lessonheading.tsx
+++ src/components/nav/lessonheading.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import { buttonVariants } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import streak from "public/streak.png";
+import blazing from "public/blazing.png";
+import ThemeButton from "./theme";
+import ChatButton from "./chatbutton";
+import { usePathname } from "next/navigation";
 
 export default function LessonHeading() {
+  const pathname = usePathname();
+
   return (
-    <div className="flex w-screen flex-row items-center justify-between border border-b-border px-10 py-1">
+    <div className="fixed left-0 right-0 top-0 z-20 flex w-screen flex-row items-center justify-between border border-b-border  bg-background py-2 shadow md:px-10">
       <Link href={"/courses/"} className={buttonVariants({ variant: "ghost" })}>
         <X className="h-4 w-4" />
       </Link>
@@ -16,8 +23,19 @@ export default function LessonHeading() {
         value={20}
         className="h-3 w-[300px] max-w-[40%] border border-border"
       />
-
-      <Image width={40} height={30} src={streak} alt="streak" />
+      <div className="flex flex-row items-center gap-2">
+        <ThemeButton className="hidden md:block" />
+        <ChatButton query={`prev=${pathname}`} lesson={true} />
+        <div className="cursor-pointer transition-all hover:scale-105">
+          <Image
+            className="m-1"
+            width={45}
+            height={45}
+            src={blazing}
+            alt="streak"
+          />
+        </div>
+      </div>
     </div>
   );
 }
