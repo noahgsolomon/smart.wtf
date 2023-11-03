c142a33c778732833b99ce5c87f26de2ac017ade
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-03 15:17:45 -0400

    lesson page now dynamic

--- src/app/lesson/[slug]/[chapter]/[lesson]/page.tsx
+++ src/app/lesson/[slug]/[chapter]/[lesson]/page.tsx
@@ -3,91 +3,46 @@ import { compileMDX } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import slug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { api } from "@/trpc/server";
+import { redirect } from "next/navigation";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
-export default async function Page() {
-  const sections = [
-    {
-      markdown: `# Introduction to Systems Design: Concepts and Importance
-    
-    Welcome to the comprehensive journey into the world of **Systems Design**. This field, essential and intricate, stands as a cornerstone in the ever-evolving landscape of technology. It's here that the blueprints of digital products and services are conceptualized, tested, and refined. In this introductory lesson, we'll explore the fundamental principles that underpin systems design and uncover the reasons why mastering this discipline is indispensable for technologists, engineers, and visionaries alike.`,
-      order: 1,
-    },
-    {
-      markdown: `
-  Systems Design is an interdisciplinary process that defines the architecture, modules, interfaces, and data for a system to meet specific requirements. It's an exercise in foresight, innovation, and precision that aims to build systems capable of not just meeting the current demands but also adapting to future changes and challenges.`,
-      order: 2,
-    },
-    {
-      markdown: `  # The Living Organism of a System
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string; chapter: string; lesson: string };
+  searchParams: { l: string };
+}) {
+  if (!searchParams.l) {
+    redirect(`?l=1`);
+  }
 
-      ![robot-surhery](https://images.codefoli.com/robot-surgery.png)
-      
-  Imagine a system as a living organism. Like any living thing, a system must grow, respond to its environment, remain healthy, and protect itself against threats. The core concepts of systems design—scalability, reliability, availability, efficiency, and security—are akin to the vital signs of this organism.
+  const section = await api.course.getCourseSection.query({
+    sectionId: parseInt(params.lesson),
+  });
 
-**Scalability** is the breath of a system, allowing it to expand and contract as necessary, accommodating an influx of users, a surge in data, or an increase in transactions without losing its composure. Scalability ensures that the system can sustain growth and change without compromising performance.
+  if (section.section?.length === 0) redirect("/404");
 
-**Reliability** is the heartbeat of a system. It is the measure of predictability and dependability, where the system performs its intended functions under designated conditions with unwavering consistency. It's about trust—ensuring that users can rely on the system to deliver results whenever and however they need them.
+  const subSection = parseInt(searchParams.l);
 
-**Availability** is the system's pulse, present and accounted for at all times. High availability means the system is operational and ready to be accessed, without unexpected downtimes or disruptions—a critical measure for systems where any amount of inaccessibility translates to lost opportunities and trust.
+  if (
+    isNaN(subSection) ||
+    subSection < 0 ||
+    subSection > (section.section?.length ?? 0)
+  ) {
+    redirect(`?l=1`);
+  }
 
-**Efficiency** reflects the system's metabolism. It's about doing more with less, maximizing output while minimizing waste—whether that waste is time, resources, or effort. An efficient system is lean and agile, able to deliver performance without unnecessary consumption of resources.
+  if (section.section === null) redirect("/404");
 
-**Security** is the immune system. In a world where threats are omnipresent and ever-changing, a secure system can defend itself against attacks, safeguard its integrity, and protect the privacy of its data and users.`,
-      order: 3,
-    },
-    {
-      markdown: `## Importance
-  
-  The importance of Systems Design transcends its technical aspects. It's a strategic imperative. As the backbone of businesses, governments, and societies, a well-designed system supports sustainable growth, drives innovation, and fosters trust. Ineffective systems design can lead to failure, breaches, and inefficiencies that have far-reaching consequences.
-
-This lesson is not just an introduction to the technicalities; it's an invitation to adopt a systems thinking mindset. We will dissect real-world case studies, engage with thought-provoking exercises, and learn to see the larger picture. By the end of this lesson, you'll grasp why Systems Design is more than a phase in the development process—it's a critical perspective that shapes the way we solve problems, create value, and impact the world through technology.
-
-As we move forward, keep in mind that Systems Design is not a static field; it is dynamic and responsive to the shifts in technology and society. What remains constant is the need for systems that are robust, flexible, and secure. Embrace the complexities and the challenges, for they are the catalysts for innovation and excellence in Systems Design.
-`,
-      order: 4,
-    },
-    {
-      markdown: `  # What we will cover:
-  
-  ### Chapter 1: Fundamentals of System Design
-This chapter lays the foundation for system design by exploring the key components and architecture of modern systems, highlighting the importance of understanding both for aspiring system designers. It delves into scalability, comparing vertical and horizontal scaling techniques, and underscores the significance of knowing how to apply them for sustainable system growth. Additionally, the chapter discusses the critical performance metrics of latency and throughput, stressing their roles in evaluating and optimizing the efficiency and responsiveness of a system.
-
-### Chapter 2: Designing Databases
-Chapter 2 provides a comprehensive guide to designing databases, starting with a comparative analysis of relational and NoSQL databases to assist in selecting the appropriate type based on system requirements. It further examines database partitioning and sharding techniques as essential tools for managing large datasets and enhancing performance. Database indexing and the exploration of modern large-scale data solutions like BigTable and DynamoDB are also covered, underlining their importance in data retrieval and storage.
-
-### Chapter 3: Network Considerations in Systems Design
-The third chapter focuses on network-related aspects of systems design, beginning with the basics of network protocols, particularly HTTP and HTTPS, which are crucial for secure system communication. Strategies for load balancing are discussed, emphasizing their role in traffic distribution and system reliability. The chapter also addresses the role of Content Delivery Networks (CDNs) and edge computing in improving content delivery, alongside the imperative need for network security and protection against DDoS attacks.
-
-### Chapter 4: Designing for High Availability
-In this chapter, readers learn about designing systems for high availability through redundancy and failover mechanisms, foundational for maintaining operations during component failures. It emphasizes the need for robust backup strategies and disaster recovery plans to ensure data integrity. Monitoring and logging are presented as crucial tools for maintaining system health, while the implementation of health checks and circuit breakers are discussed as preventative measures against systemic failures.
-
-### Chapter 5: Storage Systems and Data Management
-Chapter 5 explores the intricacies of storage systems and data management, distinguishing between block and object storage and their respective use cases. The design of efficient file systems is covered for their impact on performance and manageability. Additionally, the chapter examines data replication and consistency models, essential for the reliability of distributed systems, and concludes with a discussion on managing streaming data and real-time processing requirements.
-
-### Chapter 6: Microservices and Decentralized Architectures
-This chapter advocates for microservices over monolithic architectures by detailing their benefits in terms of flexibility and scalability. It highlights the importance of service discovery and orchestration in managing complex microservices landscapes. Event-driven architectures and message queues are presented as solutions to decouple service dependencies, and the chapter concludes by addressing common challenges associated with microservices, such as data management and latency issues.
-
-### Chapter 7: Designing for Security
-Chapter 7 delves into the key principles of system security, establishing it as a cornerstone of user trust and compliance. It covers authentication and authorization mechanisms, stressing their role in safeguarding system access. Securing data at rest and in transit is discussed to prevent unauthorized access and breaches, and the chapter wraps up with an overview of advanced mitigation techniques like rate limiting and sandboxing.
-
-### Chapter 8: Caching Strategies
-In Chapter 8, the role of caching in system design is examined for its critical impact on performance and scalability. The implementation of different caching layers, such as application-level and database-level, is discussed, along with their unique benefits. Popular caching solutions like Redis, Memcached, and Varnish are reviewed, and cache eviction strategies along with cache invalidation are discussed to ensure data accuracy and optimal resource use.
-
-### Chapter 9: Design Patterns and Best Practices
-This chapter presents commonly used design patterns in system design, providing proven solutions to typical design challenges. It underscores the importance of modular and decoupled design for system maintenance and scalability. Special considerations for designing mobile and IoT systems are discussed for their unique performance and user experience needs. The chapter concludes by advocating for continuous integration and deployment (CI/CD) as practices that enable rapid and reliable software development cycles.
-
-### Chapter 10: Real-world Case Studies
-Chapter 10 offers a pragmatic perspective by presenting real-world case studies that demonstrate the transition from monolithic to microservices architectures. It examines high-traffic events to provide insights into scalability and performance optimization under pressure. The chapter also explores multi-region deployment as a strategy for global scaling and concludes with a review of postmortem analyses that yield valuable lessons from system failures.
-  `,
-      order: 5,
-    },
-  ];
+  console.log("len", section.section.length);
 
   const { content } = await compileMDX({
-    source: sections
-      .sort((a, b) => a.order - b.order)
-      .map((s) => s.markdown)
-      .join("\n"),
+    source: section.section[subSection - 1]?.blocks
+      .map((block) => block.markdown)
+      .join("\n\n")!,
     options: {
       mdxOptions: {
         remarkPlugins: [remarkGfm],
@@ -109,6 +64,18 @@ Chapter 10 offers a pragmatic perspective by presenting real-world case studies 
       <div className="prose prose-slate relative px-8 py-2 dark:prose-invert">
         <CopyButton />
         {content}
+        {section.section.length > subSection ? (
+          <Link className={buttonVariants()} href={`?l=${subSection + 1}`}>
+            Continue
+          </Link>
+        ) : (
+          <Link
+            className={buttonVariants()}
+            href={`/courses/${params.slug}/chapter-${params.chapter}`}
+          >
+            Finish
+          </Link>
+        )}
       </div>
     </div>
   );
