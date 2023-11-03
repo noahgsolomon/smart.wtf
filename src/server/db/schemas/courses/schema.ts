9f647eb012a6c7e8778a8e8fed9fca20cafe77b3
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-03 13:24:03 -0400

    course schema so far

--- src/server/db/schemas/courses/schema.ts
+++ src/server/db/schemas/courses/schema.ts
@@ -1,6 +1,7 @@
 import {
   int,
   mysqlTable,
+  text,
   unique,
   uniqueIndex,
   varchar,
@@ -94,12 +95,113 @@ export const courseChapterSections = mysqlTable("course_chapter_sections", {
 
 export const courseChapterSectionsRelations = relations(
   courseChapterSections,
-  ({ one }) => ({
+  ({ one, many }) => ({
     courseChapters: one(courseChapters, {
       fields: [courseChapterSections.chapterId],
       references: [courseChapters.id],
     }),
+    subSections: many(subSections),
   }),
 );
+
+export const subSections = mysqlTable(
+  "sub_sections",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    sectionId: int("section_id").notNull(),
+    order: int("order").notNull(),
+    name: varchar("name", { length: 200 }).notNull(),
+    time: int("time").notNull(),
+  },
+  (t) => ({
+    unq: unique().on(t.sectionId, t.order),
+  }),
+);
+
+export const subSectionsRelations = relations(subSections, ({ one, many }) => ({
+  courseChapterSections: one(courseChapterSections, {
+    fields: [subSections.sectionId],
+    references: [courseChapterSections.id],
+  }),
+  blocks: many(blocks),
+}));
+
+export const blocks = mysqlTable(
+  "blocks",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    subSectionId: int("sub_section_id").notNull(),
+    order: int("order").notNull(),
+    markdown: text("markdown").notNull(),
+  },
+  (t) => ({
+    unq: unique().on(t.subSectionId, t.order),
+  }),
+);
+
+export const blocksRelations = relations(blocks, ({ one, many }) => ({
+  subSections: one(subSections, {
+    fields: [blocks.subSectionId],
+    references: [subSections.id],
+  }),
+  interactiveComponents: many(interactiveComponents),
+}));
+
+export const interactiveComponents = mysqlTable("interactive_components", {
+  id: int("id").primaryKey().autoincrement(),
+  blockId: int("block_id").notNull(),
+  type: varchar("type", { length: 50, enum: ["QUIZ", "QUESTION"] }).notNull(),
+  quizId: int("quiz_id"),
+  questionId: int("question_id"),
+});
+
+export const interactiveComponentsRelations = relations(
+  interactiveComponents,
+  ({ one }) => ({
+    blocks: one(blocks, {
+      fields: [interactiveComponents.blockId],
+      references: [blocks.id],
+    }),
+    quizzes: one(quizzes, {
+      fields: [interactiveComponents.quizId],
+      references: [quizzes.id],
+    }),
+    questions: one(questions, {
+      fields: [interactiveComponents.questionId],
+      references: [questions.id],
+    }),
+  }),
+);
+
+export const quizzes = mysqlTable("quizzes", {
+  id: int("id").primaryKey().autoincrement(),
+  interactiveComponentId: int("interactive_component_id").notNull(),
+  questionMarkdown: varchar("question_markdown", { length: 200 }).notNull(),
+  optionOne: varchar("option_one", { length: 500 }).notNull(),
+  optionTwo: varchar("option_two", { length: 500 }).notNull(),
+  optionThree: varchar("option_three", { length: 500 }).notNull(),
+  optionFour: varchar("option_four", { length: 500 }).notNull(),
+  explanationMarkdown: text("explanation_markdown").notNull(),
+});
+
+export const quizzesRelations = relations(quizzes, ({ one }) => ({
+  interactiveComponents: one(interactiveComponents, {
+    fields: [quizzes.interactiveComponentId],
+    references: [interactiveComponents.id],
+  }),
+}));
+
+export const questions = mysqlTable("questions", {
+  id: int("id").primaryKey().autoincrement(),
+  interactiveComponentId: int("interactive_component_id").notNull(),
+  questionMarkdown: text("question_markdown").notNull(),
+});
+
+export const questionsRelations = relations(questions, ({ one }) => ({
+  interactiveComponents: one(interactiveComponents, {
+    fields: [questions.interactiveComponentId],
+    references: [interactiveComponents.id],
+  }),
+}));
 
 // export const courseChapterSection
