552b56af20f29f1664b053cf2b996db6a889014c
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-03 17:05:03 -0400

    redirect with context from lesson/chat

--- src/app/lesson/chat/page.tsx
+++ src/app/lesson/chat/page.tsx
@@ -1,6 +1,8 @@
 import Chat from "@/components/chat";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = ({ searchParams }: { searchParams: { prev: string } }) => {
+  redirect(searchParams.prev);
   return (
     <div className="flex h-screen w-screen items-center justify-center px-5">
       <Chat page={true} className="max-w-[800px]" />
