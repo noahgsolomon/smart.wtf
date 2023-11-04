a61663172224aca304eeea9249d88259e71547ed
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-04 07:50:01 -0400

    shadnc

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-accent text-secondary-foreground hover:bg-primary/10 dark:hover:bg-accent/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         success: "bg-success text-primary shadow-sm hover:bg-success/90",
