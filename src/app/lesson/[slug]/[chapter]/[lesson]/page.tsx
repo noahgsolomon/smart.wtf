5b62dd902c21c236215e5681c420a8e33972fd80
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-08 11:29:19 -0500

    animate in lesson

--- src/app/lesson/[slug]/[chapter]/[lesson]/page.tsx
+++ src/app/lesson/[slug]/[chapter]/[lesson]/page.tsx
@@ -14,6 +14,7 @@ import LessonHeading from "@/app/lesson/components/lessonheading";
 import { useSectionContext } from "@/app/lesson/sectioncontext";
 import rehypeHighlight from "rehype-highlight";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Page({
   params,
@@ -50,153 +51,167 @@ export default function Page({
   return (
     <>
       <LessonHeading section={section} />
-      <div className="flex justify-center px-0 pb-4 pt-28 md:px-4">
-        <div className="prose prose-slate relative px-8 py-2 dark:prose-invert">
-          {/* <CopyButton /> */}
-          <div>
-            {section[lessonNumber - 1]?.blocks
-              .sort((a, b) => a.order - b.order)
-              .map((block, index) => {
-                const markdown = (
-                  <Markdown
-                    components={{
-                      img: ({ ...props }) => (
-                        <Image
-                          className="rounded-lg"
-                          src={
-                            props.src ??
-                            "https://images.codefoli.com/smartwtf.png"
-                          }
-                          alt={props.alt ?? "smartwtf"}
-                          priority={true}
-                          layout="responsive"
-                          width={1792}
-                          height={1024}
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0 }} // Start from fully transparent
+          animate={{
+            opacity: 1,
+            transition: { duration: 0.15, delay: 0.3 },
+          }}
+          exit={{
+            opacity: 0, // Fade out to fully transparent
+            transition: { duration: 0.15 }, // Fade out duration of 0.5s
+          }}
+          key={lessonNumber}
+          className="flex justify-center px-0 pb-4 pt-28 md:px-4"
+        >
+          <div className="prose prose-slate relative px-8 py-2 dark:prose-invert">
+            {/* <CopyButton /> */}
+            <div>
+              {section[lessonNumber - 1]?.blocks
+                .sort((a, b) => a.order - b.order)
+                .map((block, index) => {
+                  const markdown = (
+                    <Markdown
+                      components={{
+                        img: ({ ...props }) => (
+                          <Image
+                            className="rounded-lg"
+                            src={
+                              props.src ??
+                              "https://images.codefoli.com/smartwtf.png"
+                            }
+                            alt={props.alt ?? "smartwtf"}
+                            priority={true}
+                            layout="responsive"
+                            width={1792}
+                            height={1024}
+                          />
+                        ),
+                      }}
+                      remarkPlugins={[remarkGfm]}
+                      rehypePlugins={[
+                        slug,
+                        [
+                          rehypeAutolinkHeadings,
+                          {
+                            behavior: "wrap",
+                          },
+                        ],
+                        rehypeHighlight,
+                      ]}
+                    >
+                      {block.markdown}
+                    </Markdown>
+                  );
+                  const quiz = block.interactiveComponents.map((component) => {
+                    if (component.type === "QUIZ") {
+                      const explanation = (
+                        <Markdown
+                          components={{
+                            img: ({ ...props }) => (
+                              <Image
+                                src={
+                                  props.src ??
+                                  "https://images.codefoli.com/smartwtf.png"
+                                }
+                                alt={props.alt ?? "smartwtf"}
+                                layout="responsive"
+                              />
+                            ),
+                          }}
+                        >
+                          {component.quizzes?.explanationMarkdown ??
+                            "## no explanation right now, sorry :("}
+                        </Markdown>
+                      );
+                      const question = (
+                        <Markdown
+                          components={{
+                            img: ({ ...props }) => (
+                              <Image
+                                src={
+                                  props.src ??
+                                  "https://images.codefoli.com/smartwtf.png"
+                                }
+                                alt={props.alt ?? "smartwtf"}
+                                layout="responsive"
+                              />
+                            ),
+                          }}
+                        >
+                          {component.quizzes?.questionMarkdown ??
+                            "## no question right now, sorry :("}
+                        </Markdown>
+                      );
+                      return (
+                        <Quiz
+                          subSection={lessonNumber}
+                          params={params}
+                          blockId={block.id}
+                          completed={block.userCompletedBlocks.length > 0}
+                          key={component.quizzes?.id ?? 1}
+                          content={question}
+                          explanation={explanation}
+                          options={[
+                            component.quizzes?.optionOne ?? "",
+                            component.quizzes?.optionTwo ?? "",
+                            component.quizzes?.optionThree ?? "",
+                            component.quizzes?.optionFour ?? "",
+                          ]}
+                          answer={component.quizzes?.correctOption ?? "ONE"}
                         />
-                      ),
-                    }}
-                    remarkPlugins={[remarkGfm]}
-                    rehypePlugins={[
-                      slug,
-                      [
-                        rehypeAutolinkHeadings,
-                        {
-                          behavior: "wrap",
-                        },
-                      ],
-                      rehypeHighlight,
-                    ]}
-                  >
-                    {block.markdown}
-                  </Markdown>
-                );
-                const quiz = block.interactiveComponents.map((component) => {
-                  if (component.type === "QUIZ") {
-                    const explanation = (
-                      <Markdown
-                        components={{
-                          img: ({ ...props }) => (
-                            <Image
-                              src={
-                                props.src ??
-                                "https://images.codefoli.com/smartwtf.png"
-                              }
-                              alt={props.alt ?? "smartwtf"}
-                              layout="responsive"
-                            />
-                          ),
-                        }}
-                      >
-                        {component.quizzes?.explanationMarkdown ??
-                          "## no explanation right now, sorry :("}
-                      </Markdown>
-                    );
-                    const question = (
-                      <Markdown
-                        components={{
-                          img: ({ ...props }) => (
-                            <Image
-                              src={
-                                props.src ??
-                                "https://images.codefoli.com/smartwtf.png"
-                              }
-                              alt={props.alt ?? "smartwtf"}
-                              layout="responsive"
-                            />
-                          ),
-                        }}
-                      >
-                        {component.quizzes?.questionMarkdown ??
-                          "## no question right now, sorry :("}
-                      </Markdown>
-                    );
-                    return (
-                      <Quiz
-                        subSection={lessonNumber}
-                        params={params}
-                        blockId={block.id}
-                        completed={block.userCompletedBlocks.length > 0}
-                        key={component.quizzes?.id ?? 1}
-                        content={question}
-                        explanation={explanation}
-                        options={[
-                          component.quizzes?.optionOne ?? "",
-                          component.quizzes?.optionTwo ?? "",
-                          component.quizzes?.optionThree ?? "",
-                          component.quizzes?.optionFour ?? "",
-                        ]}
-                        answer={component.quizzes?.correctOption ?? "ONE"}
-                      />
-                    );
-                  } else {
-                    return <></>;
-                  }
-                });
+                      );
+                    } else {
+                      return <></>;
+                    }
+                  });
 
-                const blockVisible =
-                  index - 1 < 0
-                    ? true
-                    : section[lessonNumber - 1]?.blocks[index - 1] &&
-                      (section[lessonNumber - 1]?.blocks[index - 1]
-                        ?.userCompletedBlocks.length ?? 0) > 0;
+                  const blockVisible =
+                    index - 1 < 0
+                      ? true
+                      : section[lessonNumber - 1]?.blocks[index - 1] &&
+                        (section[lessonNumber - 1]?.blocks[index - 1]
+                          ?.userCompletedBlocks.length ?? 0) > 0;
 
-                return (
-                  <div
-                    key={index}
-                    className={`${blockVisible ? "" : "hidden"}`}
-                  >
-                    <div id={block.id.toString() ?? 0}>{markdown}</div>
-                    {quiz}
-                    {section[lessonNumber - 1]?.blocks.length ===
-                      block.order && (
-                      <>
-                        {section.length > lessonNumber ? (
-                          <Link href={`?l=${lessonNumber + 1}`}>
-                            <LessonButtons
-                              params={params}
-                              section={section}
-                              subSection={lessonNumber}
-                            />
-                          </Link>
-                        ) : (
-                          <a
-                            href={`/courses/${params.slug}/chapter-${params.chapter}`}
-                          >
-                            <LessonButtons
-                              params={params}
-                              section={section}
-                              subSection={lessonNumber}
-                            />
-                          </a>
-                        )}
-                      </>
-                    )}
-                  </div>
-                );
-              })}
+                  return (
+                    <div
+                      key={index}
+                      className={`${blockVisible ? "" : "hidden"}`}
+                    >
+                      <div id={block.id.toString() ?? 0}>{markdown}</div>
+                      {quiz}
+                      {section[lessonNumber - 1]?.blocks.length ===
+                        block.order && (
+                        <>
+                          {section.length > lessonNumber ? (
+                            <Link href={`?l=${lessonNumber + 1}`}>
+                              <LessonButtons
+                                params={params}
+                                section={section}
+                                subSection={lessonNumber}
+                              />
+                            </Link>
+                          ) : (
+                            <a
+                              href={`/courses/${params.slug}?chapter=${params.chapter}`}
+                            >
+                              <LessonButtons
+                                params={params}
+                                section={section}
+                                subSection={lessonNumber}
+                              />
+                            </a>
+                          )}
+                        </>
+                      )}
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
