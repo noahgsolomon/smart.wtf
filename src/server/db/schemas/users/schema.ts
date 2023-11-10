027ee779bc7c9235fb4dd38a5d042f46f6473ef0
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-09 19:59:17 -0500

    clerk is index smh

--- src/server/db/schemas/users/schema.ts
+++ src/server/db/schemas/users/schema.ts
@@ -10,24 +10,30 @@ import {
 } from "drizzle-orm/mysql-core";
 import { blocks, courseLikes, courses } from "../courses/schema";
 
-export const users = mysqlTable("users", {
-  id: int("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 200 }).notNull(),
-  password: varchar("password", { length: 100 }),
-  email: varchar("email", { length: 100 }).notNull().unique(),
-  username: varchar("username", { length: 30 }).notNull().unique(),
-  clerk_id: varchar("clerk_id", { length: 200 }).notNull().unique(),
-  subscribed: boolean("subscribed").notNull().default(false),
-  created_at: datetime("created_at", { mode: "date" })
-    .notNull()
-    .default(new Date()),
-  stripeCustomerId: varchar("stripe_customer_id", { length: 255 }),
-  stripeSubscriptionId: varchar("stripe_subscription_id", { length: 255 }),
-  stripePriceId: varchar("stripe_price_id", { length: 255 }),
-  stripeCurrentPeriodEnd: datetime("stripe_current_period_end", {
-    mode: "date",
+export const users = mysqlTable(
+  "users",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    name: varchar("name", { length: 200 }).notNull(),
+    password: varchar("password", { length: 100 }),
+    email: varchar("email", { length: 100 }).notNull().unique(),
+    username: varchar("username", { length: 30 }).notNull().unique(),
+    clerk_id: varchar("clerk_id", { length: 200 }).notNull().unique(),
+    subscribed: boolean("subscribed").notNull().default(false),
+    created_at: datetime("created_at", { mode: "date" })
+      .notNull()
+      .default(new Date()),
+    stripeCustomerId: varchar("stripe_customer_id", { length: 255 }),
+    stripeSubscriptionId: varchar("stripe_subscription_id", { length: 255 }),
+    stripePriceId: varchar("stripe_price_id", { length: 255 }),
+    stripeCurrentPeriodEnd: datetime("stripe_current_period_end", {
+      mode: "date",
+    }),
+  },
+  (t) => ({
+    clerkIdx: uniqueIndex("clerk_idx").on(t.clerk_id),
   }),
-});
+);
 
 export const usersRelations = relations(users, ({ many }) => ({
   courseLikes: many(courseLikes),
