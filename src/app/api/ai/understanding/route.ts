7ab1eef119344c086cb75258d4be78f92c091986
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-11 13:58:17 -0500

    Add route for understanding API with OpenAI integration

---
+++ src/app/api/ai/understanding/route.ts
@@ -0,0 +1,88 @@
+import { OpenAI } from "openai";
+
+type UnderstandRequest = Request & {
+  body: {
+    question: string;
+    answer: string;
+  };
+};
+export const runtime = "edge";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: UnderstandRequest) {
+  try {
+    const requestBody = await req.json();
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `A user is answering the question: "${requestBody.question}". Their answer is: "${requestBody.answer}". Please return a JSON object with a 'response' object that contains a boolean 'correct'.`,
+        },
+      ],
+      functions: [
+        {
+          name: "question_answer",
+          description: `Determines if a user's answer to a question is correct along with explanation.`,
+          parameters: {
+            type: "object",
+            properties: {
+              response: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    correct: { type: "boolean" },
+                    explanation: { type: "string" },
+                  },
+                },
+              },
+            },
+            required: ["response"],
+          },
+        },
+      ],
+      function_call: { name: "question_answer" },
+      model: "gpt-4-1106-preview",
+      response_format: { type: "json_object" },
+    });
+
+    const responseBody = JSON.parse(
+      completion.choices[0]?.message.function_call?.arguments!,
+    );
+
+    return new Response(
+      JSON.stringify({
+        status: 200,
+        body: {
+          data: {
+            correct: responseBody.response[0].correct,
+          },
+        },
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  } catch (error) {
+    console.error("Error in OpenAI API call:", error);
+    return new Response(
+      JSON.stringify({
+        status: 500,
+        body: {
+          error: "Internal Server Error",
+          message: error,
+        },
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+}
