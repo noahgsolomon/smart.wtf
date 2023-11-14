63534ea4ec5309c7e3083983063eec93d99c70c8
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-13 20:26:09 -0500

    Add latest course activity to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,22 @@
 import { api } from "@/trpc/server";
-import Streak from "./components/streak";
 import cool from "public/cool.png";
 import Image from "next/image";
-import LatestAchievements from "./components/latestachievements";
 import { buttonVariants } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
-import { MessageSquare } from "lucide-react";
+import { ArrowRightCircle } from "lucide-react";
 import CalculusOverview from "./components/calculusoverview";
 import Courses from "./components/courses/courses";
 import { Suspense } from "react";
 import { CourseSkeleton } from "./components/courses/courseskeleton";
 import { cn } from "@/lib/utils";
+import { Progress } from "@/components/ui/progress";
 
 const Dashboard = async () => {
   const user = (await api.user.user.query()).user;
+
+  const latest = await api.course.getLatestActivity.query();
+
+  console.log(latest);
 
   return (
     <>
@@ -26,7 +28,55 @@ const Dashboard = async () => {
                 <h3 className="text-4xl">Welcome back, {user?.name}</h3>
                 <Image width={50} height={50} src={cool} alt="wave emoji" />
               </div>
-              <div>
+              <div className="flex flex-col gap-2">
+                <h4>Pick up where you left off</h4>
+                <div className="flex">
+                  <div className="flex flex-col gap-4 rounded-lg border border-border p-4">
+                    <div className="">
+                      <Image
+                        src={
+                          latest.latest.courseChapterSections?.imageUrl ??
+                          "https://images.codefoli.com/systems-design-1-1.png"
+                        }
+                        alt="lesson"
+                        width={400}
+                        className="rounded-lg "
+                        height={200}
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h3>
+                        {latest.latest.courseChapterSections?.course.name ??
+                          "Systems Design"}
+                      </h3>
+                      <p className="max-w-[30ch] text-base">
+                        {latest.latest.courseChapterSections?.name ?? "Lesson"}
+                      </p>
+                      <p className="text-sm opacity-80">
+                        Lesson x of 40 {"(unimplemented)"}
+                      </p>
+                    </div>
+                    <Progress
+                      className="h-4 border-2 border-border"
+                      indicatorClassName="bg-blue rounded-r-lg"
+                      value={latest.latest.percentageCompleted ?? 50}
+                    />
+                    <Link
+                      href={`/lesson/${latest.latest.courseChapterSections
+                        ?.course.slug}/${latest.latest.courseChapterSections
+                        ?.courseChapters.order}/${latest.latest.sectionId}?l=${
+                        latest.latest.subSections?.order ?? 1
+                      }`}
+                      className={cn(buttonVariants(), `transition-all`)}
+                    >
+                      Continue where you left off{" "}
+                      <ArrowRightCircle className=" h-5 w-5 pl-1" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+
+              {/* <div>
                 <Link
                   href="/chat"
                   className={cn(
@@ -52,7 +102,7 @@ const Dashboard = async () => {
               <div className="flex max-w-[700px] flex-col gap-2 md:w-[60%]">
                 <Streak />
                 <LatestAchievements />
-              </div>
+              </div> */}
             </div>
           </section>
         </div>
