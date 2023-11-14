f925efccdd662a26b681cc6995c95ffa17c1b67f
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-14 06:02:08 -0500

    Add LatestActivitySkeleton component with Skeleton placeholders

---
+++ src/app/dashboard/components/latestactivityskeleton.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function LatestActivitySkeleton() {
+  return (
+    <div className="z-10 flex flex-col gap-2">
+      <h4>Pick up where you left off</h4>
+      <div className="flex ">
+        <div className="flex flex-col gap-4 rounded-lg border border-border bg-card p-4">
+          <div>
+            <Skeleton className="h-[229px] w-[400px]" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <Skeleton className="h-[40px] w-[200px]" />
+            <Skeleton className="h-[20px] w-[125px]" />
+            <Skeleton className="h-[10px] w-[75px]" />
+            <Skeleton className="h-[10px] w-[75px]" />
+          </div>
+          <Skeleton className="h-[15px] w-[400px]" />
+          <Skeleton className="h-[35px] w-[400px]" />
+        </div>
+      </div>
+    </div>
+  );
+}
