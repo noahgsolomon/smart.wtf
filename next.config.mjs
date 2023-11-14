9ea8d005b3efa760c1aee48d8fabef27dbc3a72f
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-14 15:49:22 -0500

    Delete unused components and update Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -4,29 +4,11 @@
  */
 /** @type {import("next").NextConfig} */
 
-import nextMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
-import slug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeHighlight from "rehype-highlight";
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [slug, rehypeAutolinkHeadings, rehypeHighlight],
-  },
-});
-
-const nextConfig = withMDX({
-  experimental: {
-    webpackBuildWorker: true,
-    mdxRs: true,
-  },
+const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     domains: ["img.icons8.con", "img.clerk.com", "images.codefoli.com"],
   },
-});
+};
 
 export default nextConfig;
