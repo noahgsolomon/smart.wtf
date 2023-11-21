537a37e7c7f53f47e37b5bc4efeb4eef2f605d9a
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-21 17:06:54 -0500

    Add sorting option to course API

--- src/server/api/routers/courses.ts
+++ src/server/api/routers/courses.ts
@@ -193,7 +193,7 @@ export const courseRouter = createTRPCRouter({
                   columns: {
                     type: true,
                   },
-                  with: { quizzes: true, understanding: true },
+                  with: { quizzes: true, understanding: true, sorting: true },
                 },
                 userCompletedBlocks: {
                   where: eq(userCompletedBlocks.userId, ctx.user_id),
