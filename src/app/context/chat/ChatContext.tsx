1f715b5a8cd28a9434dc6c9a4bcb4b827f4e8caa
Noah Solomon <noahsolomon2003@gmail.com>
2023-11-26 12:26:16 -0500

    additional chat context

--- src/app/context/chat/ChatContext.tsx
+++ src/app/context/chat/ChatContext.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { trpc } from "@/trpc/client";
+import { Message } from "@/types";
 import React, {
   createContext,
   useContext,
@@ -7,13 +9,18 @@ import React, {
   type ReactNode,
   type Dispatch,
   type SetStateAction,
+  useEffect,
 } from "react";
 
 type ChatContextType = {
   chat: string;
+  threadId: string;
+  assistantId: string;
   setChat: Dispatch<SetStateAction<string>>;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
+  messages: Message[];
+  setMessages: Dispatch<SetStateAction<Message[]>>;
 };
 
 const ChatContext = createContext<ChatContextType | undefined>(undefined);
@@ -33,9 +40,40 @@ type ChatProviderProps = {
 export const ChatProvider = ({ children }: ChatProviderProps) => {
   const [chat, setChat] = useState<string>("");
   const [open, setOpen] = useState<boolean>(false);
+  const userThreadQuery = trpc.ai.getThread.useQuery();
+
+  const threadId = userThreadQuery.data?.threadId!;
+
+  const assistantId = "asst_Z1KwKAyaA4lKWtKEiutE2ORK";
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const messageQuery = trpc.ai.getMessages.useQuery({
+    assistantId,
+    threadId,
+  });
+
+  useEffect(() => {
+    if (messages.length === 0 && open) {
+      setMessages(messageQuery.data?.messages!);
+    }
+  }, [open]);
+
+  console.log(messages);
 
   return (
-    <ChatContext.Provider value={{ chat, setChat, open, setOpen }}>
+    <ChatContext.Provider
+      value={{
+        chat,
+        setChat,
+        open,
+        setOpen,
+        assistantId,
+        threadId,
+        messages,
+        setMessages,
+      }}
+    >
       {children}
     </ChatContext.Provider>
   );
