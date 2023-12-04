fe37e104aacc8cac5a771cb3fedbe972682ff12d
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-04 10:42:26 -0500

    animating for notes

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -16,6 +16,7 @@ import remarkGfm from "remark-gfm";
 import rehypeKatex from "rehype-katex";
 import slug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { AnimatePresence, motion } from "framer-motion";
 
 type UserNote = {
   id: number;
@@ -79,99 +80,120 @@ export default function Page({ params }: { params: { noteId: string } }) {
     setUserNotes,
   ]);
 
+  if (retrieveNoteQuery.isLoading || retrieveUserNotesQuery.isLoading) {
+    return <></>;
+  }
+
+  const pageVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1, transition: { duration: 0.3 } },
+    exit: { opacity: 0, transition: { duration: 0.3 } },
+  };
+
   return (
     <>
       <NotesHeading currentNote={{ id: parseInt(params.noteId) }} />
-      <div className="flex flex-col gap-8 pt-[3rem]">
-        <div className="relative h-[250px] w-full overflow-hidden">
-          <Image
-            layout="fill"
-            objectFit="cover"
-            src={note?.imageUrl!}
-            alt={"note image"}
-            className="border-b border-border"
-          />
-        </div>
-        <div className="flex justify-center px-0 pb-4 pt-8 md:px-4">
-          <div className="relative px-8 py-2 pb-24">
-            <div className="flex flex-col gap-2">
-              <h1 className="text-5xl">{note?.title}</h1>
-              <div className="flex flex-row gap-2">
-                <p className="flex flex-row items-center gap-1 text-sm opacity-60">
-                  <Clock className="h-3 w-3" />
-                  {note?.minutes} min read
-                </p>
-                {/*@ts-ignore*/}
-                <Badge variant={note?.category.toLowerCase()}>
-                  {note?.category}
-                </Badge>
-              </div>
-              <div className="flex">
-                <div className="flex flex-row overflow-hidden rounded-lg border border-border">
-                  <Button
-                    className="rounded-br-none rounded-tr-none"
-                    onClick={() => setReadingMode("normal")}
-                    variant={readingMode === "normal" ? "default" : "ghost"}
+      <AnimatePresence>
+        <motion.div
+          variants={pageVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          <div className="flex flex-col gap-8 pt-[3rem]">
+            <div className="relative h-[250px] w-full overflow-hidden">
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={note?.imageUrl!}
+                alt={"note image"}
+                className="border-b border-border"
+              />
+            </div>
+
+            <div className="flex justify-center px-0 pb-4 pt-8 md:px-4">
+              <div className="relative px-8 py-2 pb-24">
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-5xl">{note?.title}</h1>
+                  <div className="flex flex-row gap-2">
+                    <p className="flex flex-row items-center gap-1 text-sm opacity-60">
+                      <Clock className="h-3 w-3" />
+                      {note?.minutes} min read
+                    </p>
+                    {/*@ts-ignore*/}
+                    <Badge variant={note?.category.toLowerCase()}>
+                      {note?.category}
+                    </Badge>
+                  </div>
+                  <div className="flex">
+                    <div className="flex flex-row overflow-hidden rounded-lg border border-border">
+                      <Button
+                        className="rounded-br-none rounded-tr-none"
+                        onClick={() => setReadingMode("normal")}
+                        variant={readingMode === "normal" ? "default" : "ghost"}
+                      >
+                        Normal
+                      </Button>
+                      <Button
+                        onClick={() => setReadingMode("agent")}
+                        className="rounded-bl-none rounded-tl-none"
+                        variant={readingMode === "agent" ? "default" : "ghost"}
+                      >
+                        {note?.agents.name}
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute -top-[125px] left-8 ">
+                  <Image
+                    src={note?.agents.pfp!}
+                    alt="agent"
+                    width={100}
+                    height={100}
+                    className="rounded-full border border-border bg-secondary/90"
+                  />
+                </div>
+                <div className="prose prose-slate pt-12 dark:prose-invert">
+                  <Markdown
+                    components={{
+                      img: ({ ...props }) => (
+                        <Image
+                          className="rounded-lg"
+                          src={
+                            props.src ??
+                            "https://images.codefoli.com/smartwtf.png"
+                          }
+                          alt={props.alt ?? "smartwtf"}
+                          priority={true}
+                          layout="responsive"
+                          width={1792}
+                          height={1024}
+                        />
+                      ),
+                    }}
+                    remarkPlugins={[remarkGfm, remarkMath]}
+                    rehypePlugins={[
+                      rehypeKatex,
+                      slug,
+                      [
+                        rehypeAutolinkHeadings,
+                        {
+                          behavior: "wrap",
+                        },
+                      ],
+                      rehypeHighlight,
+                    ]}
                   >
-                    Normal
-                  </Button>
-                  <Button
-                    onClick={() => setReadingMode("agent")}
-                    className="rounded-bl-none rounded-tl-none"
-                    variant={readingMode === "agent" ? "default" : "ghost"}
-                  >
-                    {note?.agents.name}
-                  </Button>
+                    {readingMode === "normal"
+                      ? note?.markdown
+                      : note?.agents_markdown}
+                  </Markdown>
                 </div>
               </div>
             </div>
-            <div className="absolute -top-[125px] left-8 ">
-              <Image
-                src={note?.agents.pfp!}
-                alt="agent"
-                width={100}
-                height={100}
-                className="rounded-full border border-border bg-secondary/90"
-              />
-            </div>
-            <div className="prose prose-slate pt-12 dark:prose-invert">
-              <Markdown
-                components={{
-                  img: ({ ...props }) => (
-                    <Image
-                      className="rounded-lg"
-                      src={
-                        props.src ?? "https://images.codefoli.com/smartwtf.png"
-                      }
-                      alt={props.alt ?? "smartwtf"}
-                      priority={true}
-                      layout="responsive"
-                      width={1792}
-                      height={1024}
-                    />
-                  ),
-                }}
-                remarkPlugins={[remarkGfm, remarkMath]}
-                rehypePlugins={[
-                  rehypeKatex,
-                  slug,
-                  [
-                    rehypeAutolinkHeadings,
-                    {
-                      behavior: "wrap",
-                    },
-                  ],
-                  rehypeHighlight,
-                ]}
-              >
-                {readingMode === "normal"
-                  ? note?.markdown
-                  : note?.agents_markdown}
-              </Markdown>
-            </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
