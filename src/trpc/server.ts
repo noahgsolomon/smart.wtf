283b585fa391a3c2d5e90e0c1f3f5062da70d369
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-05 12:53:16 -0500

    removed logging trpc

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -12,9 +12,7 @@ export const api = createTRPCProxyClient<AppRouter>({
   transformer,
   links: [
     loggerLink({
-      enabled: (op) =>
-        process.env.NODE_ENV === "development" ||
-        (op.direction === "down" && op.result instanceof Error),
+      enabled: () => false,
     }),
     unstable_httpBatchStreamLink({
       url: getUrl(),
