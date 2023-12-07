920737c35a19d81d24169a78524664a67de8c525
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-07 18:05:17 -0500

    add note component implemented

--- src/app/dashboard/components/notes/notes.tsx
+++ src/app/dashboard/components/notes/notes.tsx
@@ -1,12 +1,18 @@
 import { api } from "@/trpc/server";
 import NotesCard from "./notescard";
+import AddNote from "./addnote";
 
 const Notes = async () => {
   const notes = (await api.notes.getUserNotes.query()).notes;
 
   return (
     <div>
-      <h1>Notes</h1>
+      <div className="flex flex-row items-center gap-4">
+        <h1>Notes</h1>
+        <div>|</div>
+        <AddNote />
+      </div>
+
       <div className="flex flex-wrap gap-8 py-4">
         {notes.map((note) => {
           return <NotesCard note={note} key={note.id} />;
