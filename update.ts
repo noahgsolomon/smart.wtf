a973822b24bce1abb236b1b07772b1919e27aadc
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-08 13:29:25 -0500

    update.ts

--- update.ts
+++ update.ts
@@ -4,28 +4,48 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const validNote = async (note: string) => {
+const imagePrompt = async (title: string) => {
   try {
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
+      model: "ft:gpt-3.5-turbo-1106:personal::8TEhcfKm",
       messages: [
         {
-          role: "system",
-          content: `Evaluate the user's request for a note on the topic: "${note}". Determine whether the topic is appropriate and substantial enough for an academic or educational note. If the topic is inappropriate, offensive, lacks educational value, or is nonsensical, return a JSON object with the attribute 'valid' set to false. If the topic is appropriate and has enough depth for an in-depth exploration of 1,000+ words, return a JSON object with the following attributes: 'valid' (set to true), 'description' (a brief one-sentence description of the topic), 'title' (the suggested title for the note), and 'category' (choose from ENGLISH, MATH, SCIENCE, HISTORY, ARTS, MUSIC, LITERATURE, PHILOSOPHY, GEOGRAPHY, SOCIAL STUDIES, PHYSICAL EDUCATION, COMPUTER SCIENCE, ECONOMICS, BUSINESS STUDIES, PSYCHOLOGY, LAW, POLITICAL SCIENCE, ENVIRONMENTAL SCIENCE, ENGINEERING, MEDICINE, AGRICULTURE, ASTRONOMY).`,
+          role: "user",
+          content: title,
         },
       ],
-
-      response_format: { type: "json_object" },
     });
 
-    const argumentsData = JSON.parse(
-      response.choices[0]?.message.content ?? "{}",
-    );
-
-    return argumentsData;
+    return response.choices[0]?.message.content;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 };
 
-console.log(await validNote("hello world"));
+const imageGeneration = async (prompt: string) => {
+  const detailed8BitPreface =
+    "Create an image in a detailed retro 8-bit style. The artwork should have a pixelated texture and should have vibrant coloring and scenery.";
+
+  const fullPrompt = `${detailed8BitPreface} ${prompt} Remember, this is in retro 8-bit style`;
+
+  const responseFetch = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: fullPrompt,
+    n: 1,
+    size: "1792x1024",
+    quality: "hd",
+    style: "vivid",
+    response_format: "url",
+    user: "user-1234",
+  });
+
+  return responseFetch.data[0]?.url;
+};
+
+// console.log(await imagePrompt("Consensus Algorithms in Distributed Systems"));
+
+console.log(
+  await imageGeneration(
+    "Create an image reminiscent of ancient cave paintings, with a modern twist. In a dimly lit cave, a group of stick figures from various backgrounds and cultures stand in a circle. Each figure is holding a smartphone or tablet, and they're all tapping away on their devices, symbolizing the exchange of information and communication in a distributed system. The cave walls, adorned with primitive drawings of animals and shapes, also have digital screens displaying complex mathematical algorithms and diagrams. This blend of ancient and modern elements portrays the evolution of consensus algorithms in a whimsical and thought-provoking manner.",
+  ),
+);
