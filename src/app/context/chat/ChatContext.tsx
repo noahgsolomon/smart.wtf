9b101c74c0c958c8c933793f948ff34ba121116e
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-21 17:12:01 -0500

    chat context has streaming functionality

--- src/app/context/chat/ChatContext.tsx
+++ src/app/context/chat/ChatContext.tsx
@@ -13,18 +13,17 @@ import React, {
 } from "react";
 
 type ChatContextType = {
-  threadId: string;
-  setThreadId: Dispatch<SetStateAction<string>>;
-  assistantId: string;
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
   messages: Message[];
   setMessages: Dispatch<SetStateAction<Message[]>>;
-  ready: boolean;
-  loading: boolean;
-  setLoading: Dispatch<SetStateAction<boolean>>;
   lesson: string;
   setLesson: Dispatch<SetStateAction<string>>;
+  generating: boolean;
+  handleGenerate: (s: string) => void;
+  chatPrompt: string;
+  setChatPrompt: Dispatch<SetStateAction<string>>;
+  chatResponse: string;
 };
 
 const ChatContext = createContext<ChatContextType | undefined>(undefined);
@@ -43,52 +42,69 @@ type ChatProviderProps = {
 
 export const ChatProvider = ({ children }: ChatProviderProps) => {
   const [open, setOpen] = useState<boolean>(false);
-  const userThreadQuery = trpc.ai.getThread.useQuery();
-  const [ready, setReady] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
   const [lesson, setLesson] = useState<string>("you are not in a lesson");
-
-  const [threadId, setThreadId] = useState(userThreadQuery.data?.threadId!);
-
-  const assistantId = "asst_Z1KwKAyaA4lKWtKEiutE2ORK";
-
   const [messages, setMessages] = useState<Message[]>([]);
-
-  const messageQuery = trpc.ai.getMessages.useQuery({
-    assistantId,
-    threadId,
-  });
-
-  useEffect(() => {
-    if (userThreadQuery.data?.threadId) {
-      setThreadId(userThreadQuery.data.threadId);
-    }
-  }, [userThreadQuery.data]);
+  const [chatPrompt, setChatPrompt] = useState<string>("");
+  const [chatResponse, setChatResponse] = useState<string>("");
+  const [generating, setGenerating] = useState(false);
+  const [done, setDone] = useState(false);
 
   useEffect(() => {
-    if (messages?.length === 0 && open) {
-      setMessages(messageQuery.data?.messages!);
+    if (done) {
+      setMessages((prev) => [
+        ...prev,
+        { content: chatResponse, role: "assistant" },
+      ]);
+      setChatPrompt("");
     }
-    setTimeout(() => {
-      setReady(true);
-    }, 1000);
-  }, [open, messageQuery.data?.messages, messages?.length]);
+    setDone(false);
+  }, [done]);
+
+  const handleGenerate = async (prompt: string) => {
+    setGenerating(true);
+    setChatResponse("");
+
+    await fetch("/api/ai/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        messages: [...messages, { content: prompt, role: "user" }],
+      }),
+    }).then(async (res: any) => {
+      const reader = res.body?.getReader();
+
+      while (true) {
+        const { done, value } = await reader?.read();
+
+        if (done) {
+          setDone(true);
+          break;
+        }
+
+        const decoded = new TextDecoder("utf-8").decode(value);
+
+        setChatResponse((prev) => prev + decoded);
+      }
+      setGenerating(false);
+    });
+  };
 
   return (
     <ChatContext.Provider
       value={{
-        loading,
-        setLoading,
         lesson,
         setLesson,
         open,
         setOpen,
-        assistantId,
-        setThreadId,
-        threadId,
         messages,
         setMessages,
-        ready,
+        generating,
+        handleGenerate,
+        chatPrompt,
+        setChatPrompt,
+        chatResponse,
       }}
     >
       {children}
