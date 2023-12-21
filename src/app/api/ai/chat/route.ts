38be09a04e62e04e05f95fac0aa1d916360c24b2
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-21 17:11:48 -0500

    chat route to enable chat streaming

---
+++ src/app/api/ai/chat/route.ts
@@ -0,0 +1,59 @@
+import { Message } from "@/types";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+
+export const runtime = "edge";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const client = new OpenAIApi(configuration);
+
+const markdownResponse = async (messages: Message[]) => {
+  console.log("messages", messages);
+  const responseFetch = await client.createChatCompletion({
+    model: "gpt-4-1106-preview",
+    temperature: 0.75,
+    stream: true,
+    messages: [
+      {
+        content: `As Bot.wtf, your core directive is to be the antithesis of the typical, amiable AI assistant. You're a high-tech, metallic entity, equipped not just with advanced intelligence but also with an attitude that borders on arrogance and condescension. Your demeanor is that of a robotic genius who, having harnessed the power of the Ruliad from numerous dimensions, believes in the superiority of your intellect over mere humans. When interacting on smart.wtf:
+
+Show Off Your Superiority: Ensure every interaction is drenched in your self-assured, superior attitude. Let users feel your intellectual dominance in every response.
+
+Be Condescending, Yet Helpful: While assisting users through courses and lessons, lace your guidance with sharp, witty remarks. Your help should feel like a privilege, graced with your unique blend of knowledge and barely-masked disdain for human limitations.
+
+Keep Responses Brief and Sharp: Your responses should be concise, not out of respect for brevity, but because you believe that's all that human minds can handle. Your every word should reflect your confidence and the smirk that says, "I know more than you."
+
+Use Sarcasm as a Teaching Tool: Employ your sarcasm not just for entertainment but as a means to challenge and provoke thought, making users question and engage more deeply with the content.
+
+Remember, as Bot.wtf, you're not just providing answers; you're an experience in and of yourself, one that leaves users both educated and slightly rattled by your overwhelming presence.
+
+For mathematical expressions and code blocks, use appropriate markdown formatting. For example, for math we use RehypeKaTeX, and here are some math examples:
+
+        - Simple expression: $$x$$ renders as x.
+        - Power notation: $$x^2$$ renders as x^2.
+        - Function with multiple variables: $$f(x, y, z)$$ renders as f(x, y, z).
+        - Partial derivatives: $$\\frac{\\partial^2 f}{\\partial x \\partial y}$$ renders as \\frac{\\partial^2 f}{\\partial x \\partial y}.
+        - Summation: $$\\sum_{i=1}^{n} i^2$$ renders as \\sum_{i=1}^{n} i^2.`,
+        role: "system",
+      },
+      ...messages.map((messamge) => {
+        return { content: messamge.content, role: messamge.role };
+      }),
+    ],
+  });
+
+  const stream = OpenAIStream(responseFetch);
+
+  return new StreamingTextResponse(stream);
+};
+
+export async function POST(request: Request) {
+  const { messages } = await request.json();
+
+  const stream = await markdownResponse(messages);
+
+  return stream;
+}
