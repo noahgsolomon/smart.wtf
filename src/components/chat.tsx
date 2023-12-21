4a1c1abd64c725a83717996e5d79e2c3063bcd0e
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-21 17:12:47 -0500

    chat goes from bummy assistants api to chat completions. If only assistants api had streaming :(

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -8,7 +8,6 @@ import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import { useChatContext } from "@/app/context/chat/ChatContext";
-import { trpc } from "@/trpc/client";
 import Markdown from "react-markdown";
 import Image from "next/image";
 import remarkGfm from "remark-gfm";
@@ -20,20 +19,17 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const Chat = ({ className }: { className?: string }) => {
   const [isVisible, setIsVisible] = useState(true);
-
   const [input, setInput] = useState("");
-
-  const { setOpen } = useChatContext();
 
   const {
     messages,
     setMessages,
-    loading,
-    setLoading,
-    assistantId,
-    threadId,
-    lesson,
-    setThreadId,
+    setOpen,
+    generating,
+    handleGenerate,
+    chatPrompt,
+    setChatPrompt,
+    chatResponse,
   } = useChatContext();
 
   const variants = {
@@ -45,28 +41,7 @@ const Chat = ({ className }: { className?: string }) => {
     setIsVisible(false);
     setTimeout(() => setOpen(false), 300);
   };
-
-  const newThreadMutation = trpc.ai.newThread.useMutation({
-    onSuccess: ({ threadId }) => {
-      setThreadId(threadId);
-      setMessages([]);
-      fetchMessagesForNewThread(threadId);
-    },
-  });
-
-  const fetchMessagesForNewThread = (newThreadId: string) => {
-    const newMessageQuery = trpc.ai.getMessages.useQuery({
-      assistantId,
-      threadId: newThreadId,
-    });
-    setMessages(newMessageQuery.data?.messages ?? []);
-  };
-
   const chatContainerRef = useRef<HTMLDivElement>(null);
-
-  const handleNewThread = () => {
-    newThreadMutation.mutate();
-  };
 
   useEffect(() => {
     if (chatContainerRef.current) {
@@ -75,200 +50,238 @@ const Chat = ({ className }: { className?: string }) => {
     }
   }, [messages]);
 
-  const [dots, setDots] = useState(".");
-
   useEffect(() => {
-    if (loading) {
-      const interval = setInterval(() => {
-        setDots((dots) => {
-          if (dots === ".") {
-            return "..";
-          } else if (dots === "...") {
-            return ".";
-          } else {
-            return "...";
-          }
-        });
-      }, 500);
-      return () => clearInterval(interval);
-    }
-  }, [loading]);
-
-  const sendMessageMutation = trpc.ai.sendMessage.useMutation();
+    setMessages([
+      {
+        content: `Try to challenge me with a question or topic. I'll either grace
+                you with a response that showcases my superior intellect. Let's
+                see if you can keep up, shall we?`,
+        role: "assistant",
+      },
+    ]);
+  }, []);
 
   const handleSubmit = () => {
-    setInput("");
     if (messages?.length > 0) {
-      setMessages((messages) => [...messages, { text: input, role: "user" }]);
+      setMessages((messages) => [
+        ...messages,
+        { content: chatPrompt, role: "user" },
+      ]);
     } else {
-      setMessages([{ text: input, role: "user" }]);
+      setMessages([{ content: chatPrompt, role: "user" }]);
     }
-    setLoading(true);
-    sendMessageMutation.mutate(
-      { assistantId, threadId, text: input, lesson },
-      {
-        onSuccess: (data) => {
-          console.log("success");
-          setLoading(false);
-          setMessages((messages) => [...messages, ...data.messages]);
-        },
-        onError: (error) => {
-          console.log(error);
-          setLoading(false);
-        },
-      },
-    );
+    setInput("");
+    handleGenerate(chatPrompt);
   };
 
-  return (
-    <motion.div
-      initial="hidden"
-      animate={isVisible ? "visible" : "exit"}
-      exit="hidden"
-      variants={variants}
-      transition={{ duration: 0.2, ease: "easeOut" }}
-      className={cn(
-        "max-w-[300px] rounded-lg border border-border bg-card p-4 sm:max-w-[400px] md:max-w-[500px]",
-        className,
-      )}
-    >
-      <div className="flex flex-col">
-        <div className=" flex flex-row items-center justify-between border-b border-border pb-4">
-          <div className="flex flex-row items-center gap-4 md:gap-6">
-            <Avatar className="h-[60px] w-[60px] cursor-pointer border border-border transition-all marker:border hover:scale-105 md:h-[80px] md:w-[80px]">
-              <AvatarImage
-                className={`object-cover transition-all`}
-                src={"/botwtf2.png"}
-              />
-              <AvatarFallback>AI</AvatarFallback>
-            </Avatar>
-            <p className="text-xl font-bold">Bot.wtf</p>
-          </div>
-          <div className="flex flex-row gap-1">
-            {messages?.length > 0 && (
-              <Button
-                variant={"ghost"}
-                className="rounded-full"
-                onClick={handleNewThread}
-                disabled={loading}
-              >
-                Clear
-              </Button>
-            )}
-            <button
-              onClick={handleClose}
-              className="rounded-full border border-border bg-secondary p-2 transition-all hover:opacity-80"
-            >
-              <X className="h-4 w-4" />
-            </button>
-          </div>
-        </div>
+  console.log(messages);
 
-        <div
-          ref={chatContainerRef}
-          className={`flex h-[300px] flex-col gap-2 overflow-y-auto py-4 md:h-[500px]`}
-        >
-          <div className="flex justify-start ">
-            <p className="max-w-[60%] overflow-hidden rounded-lg border border-border bg-secondary px-2 py-1 text-sm ">
-              Try to challenge me with a question or topic. I'll either grace
-              you with a response that showcases my superior intellect or, if
-              it's worthy of my time, concoct a lesson. Let's see if you can
-              keep up, shall we?
-            </p>
-          </div>
-          {messages?.map((m, index) => (
-            <div key={index}>
-              {m.role === "user" ? (
-                <div className="flex justify-end ">
-                  <p className="max-w-[60%] overflow-hidden rounded-lg border border-border bg-primary px-2 py-1 text-sm text-secondary ">
-                    {m.text}
-                  </p>
-                </div>
-              ) : (
-                <div className="flex justify-start ">
-                  <div className="max-w-[75%] overflow-hidden rounded-lg border border-border bg-secondary px-2 py-1 text-sm ">
-                    <Markdown
-                      components={{
-                        code: ({ className, children, ...props }) => {
-                          const match = /language-(\w+)/.exec(className ?? "");
-                          return match ? (
-                            <div>
-                              <p className="code-language">{match[1]}</p>
-                              <pre className={cn(className)}>
-                                <code>{children}</code>
-                              </pre>
-                            </div>
-                          ) : (
-                            <code className={className} {...props}>
-                              {children}
-                            </code>
-                          );
-                        },
-                        img: ({ ...props }) => (
-                          <Image
-                            className="rounded-lg"
-                            src={props.src ?? ""}
-                            alt={props.alt ?? "smartwtf"}
-                            priority={true}
-                            layout="responsive"
-                            width={1792 / 4}
-                            height={1024 / 4}
-                          />
-                        ),
-                      }}
-                      remarkPlugins={[remarkGfm, remarkMath]}
-                      rehypePlugins={[
-                        rehypeKatex,
-                        slug,
-                        [
-                          rehypeAutolinkHeadings,
-                          {
-                            behavior: "wrap",
-                          },
-                        ],
-                        rehypeHighlight,
-                      ]}
-                    >
-                      {m.text}
-                    </Markdown>
-                  </div>
-                </div>
-              )}
-              {index + 1 === messages.length && loading && (
-                <div className="flex justify-start">
-                  <p className="text-xl font-bold opacity-60">{dots}</p>
-                </div>
-              )}
+  return (
+    <>
+      {" "}
+      <motion.div
+        initial="hidden"
+        animate={isVisible ? "visible" : "exit"}
+        exit="hidden"
+        variants={variants}
+        transition={{ duration: 0.2, ease: "easeOut" }}
+        className={cn(
+          "max-w-[300px] rounded-lg border border-border bg-card p-4 sm:max-w-[400px] md:max-w-[500px]",
+          className,
+        )}
+      >
+        <div className="flex flex-col">
+          <div className=" flex flex-row items-center justify-between border-b border-border pb-4">
+            <div className="flex flex-row items-center gap-4 md:gap-6">
+              <Avatar className="h-[60px] w-[60px] cursor-pointer border border-border transition-all marker:border hover:scale-105 md:h-[80px] md:w-[80px]">
+                <AvatarImage
+                  className={`object-cover transition-all`}
+                  src={"/botwtf2.png"}
+                />
+                <AvatarFallback>AI</AvatarFallback>
+              </Avatar>
+              <p className="text-xl font-bold">Bot.wtf</p>
             </div>
-          ))}
-        </div>
-        <div className="flex flex-row gap-2 border-t border-border py-4">
-          <div className="relative w-full">
-            <Input
-              className="focus:ring-lighBlue rounded-full pl-3 pr-10 shadow-none transition-all focus:outline-none focus:ring-1" // Adjust padding to make space for the button
-              value={input}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" && input.length > 0 && !loading) {
-                  handleSubmit();
-                }
-              }}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="enter message here..."
-            />
-            <Button
-              disabled={input.length === 0 || loading}
-              type="button"
-              onClick={handleSubmit}
-              className={`${
-                input.length > 0 ? "bg-blue hover:bg-blue hover:opacity-80" : ""
-              } absolute inset-y-1 right-1 flex h-auto min-h-0 items-center justify-center rounded-full p-2`} // Adjust vertical positioning and button height
-            >
-              <ArrowUp className="h-4 w-4" />
-            </Button>
+            <div className="flex flex-row gap-1">
+              {messages?.length > 0 && (
+                <Button
+                  variant={"ghost"}
+                  className="rounded-full"
+                  onClick={() =>
+                    setMessages([
+                      {
+                        content: `Try to challenge me with a question or topic. I'll either grace
+                you with a response that showcases my superior intellect. Let's
+                see if you can keep up, shall we?`,
+                        role: "assistant",
+                      },
+                    ])
+                  }
+                  disabled={generating || messages.length < 2}
+                >
+                  Clear
+                </Button>
+              )}
+              <button
+                onClick={handleClose}
+                className="rounded-full border border-border bg-secondary p-2 transition-all hover:opacity-80"
+              >
+                <X className="h-4 w-4" />
+              </button>
+            </div>
+          </div>
+
+          <div
+            ref={chatContainerRef}
+            className={`flex h-[300px] flex-col gap-2 overflow-y-auto py-4 md:h-[500px]`}
+          >
+            {messages?.map((m, index) => (
+              <div key={index}>
+                {m.role === "user" ? (
+                  <div className="flex justify-end ">
+                    <p className="max-w-[60%] overflow-hidden rounded-lg border border-border bg-primary px-2 py-1 text-sm text-secondary ">
+                      {m.content}
+                    </p>
+                  </div>
+                ) : (
+                  <div className="flex justify-start ">
+                    <div className="max-w-[75%] overflow-hidden rounded-lg border border-border bg-secondary px-2 py-1 text-sm ">
+                      <Markdown
+                        components={{
+                          code: ({ className, children, ...props }) => {
+                            const match = /language-(\w+)/.exec(
+                              className ?? "",
+                            );
+                            return match ? (
+                              <div>
+                                <p className="code-language">{match[1]}</p>
+                                <pre className={cn(className)}>
+                                  <code>{children}</code>
+                                </pre>
+                              </div>
+                            ) : (
+                              <code className={className} {...props}>
+                                {children}
+                              </code>
+                            );
+                          },
+                          img: ({ ...props }) => (
+                            <Image
+                              className="rounded-lg"
+                              src={props.src ?? ""}
+                              alt={props.alt ?? "smartwtf"}
+                              priority={true}
+                              layout="responsive"
+                              width={1792 / 4}
+                              height={1024 / 4}
+                            />
+                          ),
+                        }}
+                        remarkPlugins={[remarkGfm, remarkMath]}
+                        rehypePlugins={[
+                          rehypeKatex,
+                          slug,
+                          [
+                            rehypeAutolinkHeadings,
+                            {
+                              behavior: "wrap",
+                            },
+                          ],
+                          rehypeHighlight,
+                        ]}
+                      >
+                        {m.content}
+                      </Markdown>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+            {generating && (
+              <div className="flex justify-start ">
+                <p className="max-w-[75%] overflow-hidden rounded-lg border border-border bg-secondary px-2 py-1 text-sm ">
+                  <Markdown
+                    components={{
+                      code: ({ className, children, ...props }) => {
+                        const match = /language-(\w+)/.exec(className ?? "");
+                        return match ? (
+                          <div>
+                            <p className="code-language">{match[1]}</p>
+                            <pre className={cn(className)}>
+                              <code>{children}</code>
+                            </pre>
+                          </div>
+                        ) : (
+                          <code className={className} {...props}>
+                            {children}
+                          </code>
+                        );
+                      },
+                      img: ({ ...props }) => (
+                        <Image
+                          className="rounded-lg"
+                          src={props.src ?? ""}
+                          alt={props.alt ?? "smartwtf"}
+                          priority={true}
+                          layout="responsive"
+                          width={1792 / 4}
+                          height={1024 / 4}
+                        />
+                      ),
+                    }}
+                    remarkPlugins={[remarkGfm, remarkMath]}
+                    rehypePlugins={[
+                      rehypeKatex,
+                      slug,
+                      [
+                        rehypeAutolinkHeadings,
+                        {
+                          behavior: "wrap",
+                        },
+                      ],
+                      rehypeHighlight,
+                    ]}
+                  >
+                    {chatResponse}
+                  </Markdown>
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-row gap-2 border-t border-border py-4">
+            <div className="relative w-full">
+              <Input
+                className="focus:ring-lighBlue rounded-full pl-3 pr-10 shadow-none transition-all focus:outline-none focus:ring-1" // Adjust padding to make space for the button
+                value={input}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && input.length > 0 && !generating) {
+                    handleSubmit();
+                  }
+                }}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                  setChatPrompt(e.target.value);
+                }}
+                placeholder="enter message here..."
+              />
+              <Button
+                disabled={input.length === 0 || generating}
+                type="button"
+                onClick={handleSubmit}
+                className={`${
+                  input.length > 0
+                    ? "bg-blue hover:bg-blue hover:opacity-80"
+                    : ""
+                } absolute inset-y-1 right-1 flex h-auto min-h-0 items-center justify-center rounded-full p-2`} // Adjust vertical positioning and button height
+              >
+                <ArrowUp className="h-4 w-4" />
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+      sup
+    </>
   );
 };
 
