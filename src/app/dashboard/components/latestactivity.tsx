ec2fef73ab38093f1c9eae4a9bb0389befff3f50
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-25 10:36:12 -0500

    latestactivity changes for resizable

--- src/app/dashboard/components/latestactivity.tsx
+++ src/app/dashboard/components/latestactivity.tsx
@@ -24,80 +24,76 @@ export default function LatestActivity() {
   }
 
   return (
-    <div className="flex w-full flex-col items-center gap-2 md:w-auto">
-      <div className="flex w-full">
-        <div className="flex w-full flex-col gap-4 rounded-lg border border-border bg-card p-4 md:w-auto">
-          <div className="w-full md:w-[400px]">
-            {latestQuery.isLoading || !latest ? (
-              <AspectRatio ratio={7 / 4}>
-                <Skeleton className={`h-full w-full rounded-lg`}></Skeleton>
-              </AspectRatio>
-            ) : (
-              <Image
-                src={
-                  latest.latest.courseChapterSections?.imageUrl ??
-                  "https://images.codefoli.com/systems-design-1-1.png"
-                }
-                priority={true}
-                alt="lesson"
-                width={700}
-                height={200}
-                className="w-full rounded-lg"
-              />
-            )}
-          </div>
-
-          <div className="flex flex-col gap-2">
-            {latestQuery.isLoading || !latest ? (
-              <>
-                <Skeleton className="h-12 w-24 rounded-lg"></Skeleton>
-                <Skeleton className="h-6 w-[75%] rounded-lg"></Skeleton>
-                <Skeleton className="h-6 w-12 rounded-lg"></Skeleton>
-              </>
-            ) : (
-              <>
-                <h3>
-                  {latest.latest.courseChapterSections?.course.name ??
-                    "Systems Design"}
-                </h3>
-                <p className="max-w-[30ch] text-base">
-                  {latest.latest.courseChapterSections?.name ?? "Lesson"}
-                </p>
-                <p className="text-sm opacity-80">
-                  Lesson {latest.latest.courseChapterSections?.lessonNumber} of{" "}
-                  {latest.latest.courseChapterSections?.course.lessons}
-                </p>
-              </>
-            )}
-          </div>
-          {latestQuery.isLoading || !latest ? (
-            <>
-              <Skeleton className="h-6 w-full rounded-full"></Skeleton>
-              <Skeleton className="h-8 w-full rounded-lg"></Skeleton>
-            </>
-          ) : (
-            <>
-              <Progress
-                className="h-4 border-2 border-border"
-                indicatorClassName="bg-blue rounded-r-lg"
-                value={latest.latest.percentageCompleted ?? 50}
-              />
-              <Link
-                onClick={() => click()}
-                href={`/lesson/${latest.latest.courseChapterSections?.course
-                  .slug}/${latest.latest.courseChapterSections?.courseChapters
-                  .order}/${latest.latest.sectionId}?l=${
-                  latest.latest.subSections?.order ?? 1
-                }&b=${latest.latest.blockId}`}
-                className={cn(buttonVariants(), `transition-all`)}
-              >
-                Continue where you left off{" "}
-                <ArrowRightCircle className=" h-5 w-5 pl-1" />
-              </Link>
-            </>
-          )}
-        </div>
+    <div className="flex w-full flex-col gap-4 bg-card p-4 md:w-auto">
+      <div className="w-full">
+        {latestQuery.isLoading || !latest ? (
+          <AspectRatio ratio={7 / 4}>
+            <Skeleton className={`h-full w-full rounded-lg`}></Skeleton>
+          </AspectRatio>
+        ) : (
+          <Image
+            src={
+              latest.latest.courseChapterSections?.imageUrl ??
+              "https://images.codefoli.com/systems-design-1-1.png"
+            }
+            priority={true}
+            alt="lesson"
+            width={700}
+            height={200}
+            className="max-w-full rounded-lg"
+          />
+        )}
       </div>
+
+      <div className="flex flex-col gap-2">
+        {latestQuery.isLoading || !latest ? (
+          <>
+            <Skeleton className="h-12 w-24 rounded-lg"></Skeleton>
+            <Skeleton className="h-6 w-[75%] rounded-lg"></Skeleton>
+            <Skeleton className="h-6 w-12 rounded-lg"></Skeleton>
+          </>
+        ) : (
+          <>
+            <h3>
+              {latest.latest.courseChapterSections?.course.name ??
+                "Systems Design"}
+            </h3>
+            <p className="max-w-[30ch] text-base">
+              {latest.latest.courseChapterSections?.name ?? "Lesson"}
+            </p>
+            <p className="text-sm opacity-80">
+              Lesson {latest.latest.courseChapterSections?.lessonNumber} of{" "}
+              {latest.latest.courseChapterSections?.course.lessons}
+            </p>
+          </>
+        )}
+      </div>
+      {latestQuery.isLoading || !latest ? (
+        <>
+          <Skeleton className="h-6 w-full rounded-full"></Skeleton>
+          <Skeleton className="h-8 w-full rounded-lg"></Skeleton>
+        </>
+      ) : (
+        <>
+          <Progress
+            className="h-4 border-2 border-border"
+            indicatorClassName="bg-blue rounded-r-lg"
+            value={latest.latest.percentageCompleted ?? 50}
+          />
+          <Link
+            onClick={() => click()}
+            href={`/lesson/${latest.latest.courseChapterSections?.course
+              .slug}/${latest.latest.courseChapterSections?.courseChapters
+              .order}/${latest.latest.sectionId}?l=${
+              latest.latest.subSections?.order ?? 1
+            }&b=${latest.latest.blockId}`}
+            className={cn(buttonVariants(), `transition-all`)}
+          >
+            Continue where you left off{" "}
+            <ArrowRightCircle className=" h-5 w-5 pl-1" />
+          </Link>
+        </>
+      )}
     </div>
   );
 }
