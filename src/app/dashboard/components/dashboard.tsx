72a0ccae54b5441224984bf146c3979ef49c7882
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-25 12:19:47 -0500

    dashboard resizable with learning path

--- src/app/dashboard/components/dashboard.tsx
+++ src/app/dashboard/components/dashboard.tsx
@@ -7,33 +7,48 @@ import {
 import LatestActivity from "./latestactivity";
 import NotesMenu from "@/app/notes/components/notesmenu";
 import Streak from "./streak";
+import LearningPath from "./learningpath/learningpath";
 
 export default function Dashboard() {
   return (
     <div>
       <ResizablePanelGroup
-        direction="vertical"
-        className="max-w-[800px] rounded-lg border"
+        direction="horizontal"
+        className="w-full rounded-lg border bg-card"
       >
-        <ResizablePanelGroup
-          direction="horizontal"
-          className="min-h-[200px] max-w-[800px] "
+        <ResizablePanel defaultSize={65}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanelGroup
+              direction="horizontal"
+              className="w-full min-w-[600px]"
+            >
+              <ResizablePanel className="min-w-[320px]">
+                <LatestActivity />
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel className="min-w-[320px] bg-card">
+                <NotesMenu />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+            <ResizablePanelGroup
+              className="max-h-[175px] border-t"
+              direction="horizontal"
+            >
+              <ResizablePanel>
+                <div className="flex w-full flex-col gap-2">
+                  <Streak />
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel
+          defaultSize={35}
+          className="min-w-[250px] max-w-[400px]"
         >
-          <ResizablePanel className="min-w-[320px]">
-            <LatestActivity />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel className="min-w-[320px] bg-card">
-            <NotesMenu />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-        <ResizablePanelGroup className="border-t" direction="horizontal">
-          <ResizablePanel>
-            <div className="flex w-full flex-col gap-2">
-              <Streak />
-            </div>
-          </ResizablePanel>
-        </ResizablePanelGroup>
+          <LearningPath />
+        </ResizablePanel>
       </ResizablePanelGroup>
     </div>
   );
