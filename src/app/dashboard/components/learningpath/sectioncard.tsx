def597a918361bb3d0e485a90795f4d2ac4df2b9
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-27 18:29:39 -0500

    Refactor SectionCard component to accept name and imageUrl as props

--- src/app/dashboard/components/learningpath/sectioncard.tsx
+++ src/app/dashboard/components/learningpath/sectioncard.tsx
@@ -2,20 +2,24 @@
 
 import Image from "next/image";
 
-export default function SectionCard() {
+export default function SectionCard({
+  name,
+  imageUrl,
+}: {
+  name: string;
+  imageUrl: string;
+}) {
   return (
-    <div className="flex min-w-[250px] flex-row gap-1 rounded-lg border p-2">
+    <div className="flex min-w-[250px] flex-row items-center gap-4 rounded-lg border p-2">
       <Image
-        src={"/machinelearning.png"}
+        src={imageUrl}
         alt="machine learning"
-        width={100}
-        height={100}
-        className="h-full rounded-lg border"
+        width={80}
+        height={80}
+        className="h-[64px] w-[64px] rounded-lg border md:h-[80px] md:w-[80px]"
       />
       <div>
-        <p className="flex flex-nowrap overflow-x-auto text-sm font-bold">
-          Introduction to Machine Learning Some Some Some
-        </p>
+        <p className="text-sm font-bold md:text-base">{name}</p>
       </div>
     </div>
   );
