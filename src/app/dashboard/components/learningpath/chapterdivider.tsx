a1b359e0194264c47f636eef49fec008f2333250
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-27 18:29:45 -0500

    Refactor ChapterDivider component to accept chapter, title, and imageUrl as props

--- src/app/dashboard/components/learningpath/chapterdivider.tsx
+++ src/app/dashboard/components/learningpath/chapterdivider.tsx
@@ -1,8 +1,16 @@
-export default function ChapterDivider() {
+export default function ChapterDivider({
+  chapter,
+  title,
+  imageUrl,
+}: {
+  chapter: number;
+  title: string;
+  imageUrl: string;
+}) {
   return (
     <div className="pb-2">
-      <p className="text-lg text-primary/60">Chapter 1</p>
-      <h4>Introduction to Machine Learning</h4>
+      <p className="text-lg text-primary/60">Chapter {chapter}</p>
+      <h4>{title}</h4>
     </div>
   );
 }
