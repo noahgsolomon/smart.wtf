0a969cf26c7418f05b9f81f9e6b8a1c4c3218c3d
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-27 18:29:42 -0500

    Fix bug in login functionality

--- src/app/dashboard/components/learningpath/learningpath.tsx
+++ src/app/dashboard/components/learningpath/learningpath.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
@@ -10,14 +11,20 @@ import Image from "next/image";
 import SectionCard from "./sectioncard";
 import ChapterDivider from "./chapterdivider";
 import { Dot } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { useEffect, useState } from "react";
 
 export default function LearningPath() {
   return (
     <div className="relative">
-      <div className="absolute bottom-0 left-0 right-0 top-0 z-20 flex items-center justify-center rounded-lg bg-primary/30">
-        <h2 className="rounded-lg border bg-card/60 p-2">Coming soon...</h2>
-      </div>
-      <div className="hidden h-full rounded-lg border bg-card p-4 2xl:block">
+      <Badge
+        className="absolute -right-6 -top-2 z-10 p-1 text-sm"
+        variant={"history"}
+      >
+        Coming Soon
+      </Badge>
+      {/* <div className="absolute bottom-0 left-0 right-0 top-0 z-20 flex items-center justify-center rounded-lg bg-primary/30"></div> */}
+      <div className="hidden h-full rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 2xl:block">
         <Carousel className="flex h-full w-full flex-col justify-between">
           <CarouselContent>
             {Array.from({ length: 3 }).map((_, index) => (
@@ -37,33 +44,73 @@ export default function LearningPath() {
                 </div>
                 <div className="h-[390px] overflow-y-auto border-b">
                   <div className="flex flex-col gap-2 py-4">
-                    <ChapterDivider />
-                    <SectionCard />
-                    <SectionCard />
-                    <SectionCard />
+                    <ChapterDivider
+                      chapter={1}
+                      title={"Foundations  of Machine Learning"}
+                      imageUrl={""}
+                    />
+                    <SectionCard
+                      name={"Introduction to Machine Learning Concepts"}
+                      imageUrl={"/ml-1-1.png"}
+                    />
+                    <SectionCard
+                      name={'"Data Preprocessing Essentials'}
+                      imageUrl={"/ml-1-2.png"}
+                    />
+                    <SectionCard
+                      name={"Supervised vs Unsupervised Learning"}
+                      imageUrl={"/ml-1-3.png"}
+                    />
                   </div>
                   <div className="flex flex-col gap-2 py-4">
-                    <ChapterDivider />
-                    <SectionCard />
-                    <SectionCard />
-                    <SectionCard />
+                    <ChapterDivider
+                      chapter={2}
+                      title={"Supervised Learning Algorithms"}
+                      imageUrl={""}
+                    />
+                    <SectionCard
+                      name={"Linear Regression Techniques"}
+                      imageUrl={"/ml-2-1.png"}
+                    />
+                    <SectionCard
+                      name={"Classification with Logistic Regression"}
+                      imageUrl={"/ml-2-2.png"}
+                    />
+                    <SectionCard
+                      name={"Decision Trees and Random Forests"}
+                      imageUrl={"/ml-2-3.png"}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2 py-4">
+                    <ChapterDivider
+                      chapter={3}
+                      title={"Unsupervised Learning"}
+                      imageUrl={""}
+                    />
+                    <SectionCard
+                      name={"Clustering with K-Means"}
+                      imageUrl={"/ml-3-1.png"}
+                    />
+                    <SectionCard
+                      name={"Dimensionality Reduction with PCA"}
+                      imageUrl={"/ml-3-2.png"}
+                    />
+                    <SectionCard
+                      name={"Neural Networks and Deep Learning Basics"}
+                      imageUrl={"/ml-3-3.png"}
+                    />
                   </div>
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <div className="flex h-[50px] w-full items-center justify-between bg-card p-2">
+          <div className="flex h-[50px] w-full items-center justify-between  p-2 ">
             <CarouselPrevious />
-            <div className="flex flex-row gap-2">
-              <Dot className="h-5 w-5 text-primary/30 transition-all" />
-              <Dot className="h-5 w-5 text-primary transition-all" />
-              <Dot className="h-5 w-5 text-primary/30 transition-all" />
-            </div>
             <CarouselNext />
           </div>
         </Carousel>
       </div>
-      <div className="hidden w-full rounded-lg border bg-card p-4 shadow-md md:block 2xl:hidden">
+      <div className="hidden w-full rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 md:block 2xl:hidden">
         <Carousel className="w-full">
           <CarouselContent className="flex flex-row">
             {Array.from({ length: 3 }).map((_, index) => (
@@ -85,41 +132,80 @@ export default function LearningPath() {
                 </div>
                 <div className="flex max-h-[350px] flex-col gap-2 overflow-y-auto border-b">
                   <div className="flex flex-col gap-2 py-4">
-                    <ChapterDivider />
+                    <ChapterDivider
+                      chapter={1}
+                      title={"Foundations  of Machine Learning"}
+                      imageUrl={""}
+                    />
                     <div className="flex max-w-full flex-row gap-2 overflow-x-auto">
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
+                      <SectionCard
+                        name={"Introduction to Machine Learning Concepts"}
+                        imageUrl={"/ml-1-1.png"}
+                      />
+                      <SectionCard
+                        name={'"Data Preprocessing Essentials'}
+                        imageUrl={"/ml-1-2.png"}
+                      />
+                      <SectionCard
+                        name={"Supervised vs Unsupervised Learning"}
+                        imageUrl={"/ml-1-3.png"}
+                      />
                     </div>
                   </div>
                   <div className="flex flex-col gap-2 py-4">
-                    <ChapterDivider />
+                    <ChapterDivider
+                      chapter={2}
+                      title={"Supervised Learning Algorithms"}
+                      imageUrl={""}
+                    />
+
                     <div className="flex max-w-full flex-row gap-2 overflow-x-auto">
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
+                      <SectionCard
+                        name={"Linear Regression Techniques"}
+                        imageUrl={"/ml-2-1.png"}
+                      />
+                      <SectionCard
+                        name={"Classification with Logistic Regression"}
+                        imageUrl={"/ml-2-2.png"}
+                      />
+                      <SectionCard
+                        name={"Decision Trees and Random Forests"}
+                        imageUrl={"/ml-2-3.png"}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 py-4">
+                    <ChapterDivider
+                      chapter={3}
+                      title={"Unsupervised Learning"}
+                      imageUrl={""}
+                    />
+                    <div className="flex max-w-full flex-row gap-2 overflow-x-auto">
+                      <SectionCard
+                        name={"Clustering with K-Means"}
+                        imageUrl={"/ml-3-1.png"}
+                      />
+                      <SectionCard
+                        name={"Dimensionality Reduction with PCA"}
+                        imageUrl={"/ml-3-2.png"}
+                      />
+                      <SectionCard
+                        name={"Neural Networks and Deep Learning Basics"}
+                        imageUrl={"/ml-3-3.png"}
+                      />
                     </div>
                   </div>
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <div className="flex h-[50px] w-full items-center justify-between bg-card p-2">
+          <div className="flex h-[50px] w-full items-center justify-between p-2">
             <CarouselPrevious />
-            <div className="flex flex-row justify-center">
-              <Dot className="h-5 w-5 text-primary/30 transition-all" />
-              <Dot className="h-5 w-5 text-primary transition-all" />
-              <Dot className="h-5 w-5 text-primary/30 transition-all" />
-            </div>
             <CarouselNext />
           </div>
         </Carousel>
       </div>
-      <div className="w-full rounded-lg border bg-card p-4 shadow-md md:hidden">
+      <div className="w-full rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 md:hidden">
         <Carousel className="w-full">
           <CarouselContent className="flex flex-row">
             {Array.from({ length: 3 }).map((_, index) => (
@@ -141,36 +227,74 @@ export default function LearningPath() {
                 </div>
                 <div className="flex max-h-[350px] flex-col gap-2 overflow-y-auto border-b">
                   <div className="flex flex-col gap-2 py-4">
-                    <ChapterDivider />
+                    <ChapterDivider
+                      chapter={1}
+                      title={"Foundations  of Machine Learning"}
+                      imageUrl={""}
+                    />
                     <div className="flex max-w-full flex-row gap-2 overflow-x-auto">
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
+                      <SectionCard
+                        name={"Introduction to Machine Learning Concepts"}
+                        imageUrl={"/ml-1-1.png"}
+                      />
+                      <SectionCard
+                        name={'"Data Preprocessing Essentials'}
+                        imageUrl={"/ml-1-2.png"}
+                      />
+                      <SectionCard
+                        name={"Supervised vs Unsupervised Learning"}
+                        imageUrl={"/ml-1-3.png"}
+                      />
                     </div>
                   </div>
                   <div className="flex flex-col gap-2 py-4">
-                    <ChapterDivider />
+                    <ChapterDivider
+                      chapter={2}
+                      title={"Supervised Learning Algorithms"}
+                      imageUrl={""}
+                    />
                     <div className="flex max-w-full flex-row gap-2 overflow-x-auto">
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
-                      <SectionCard />
+                      <SectionCard
+                        name={"Linear Regression Techniques"}
+                        imageUrl={"/ml-2-1.png"}
+                      />
+                      <SectionCard
+                        name={"Classification with Logistic Regression"}
+                        imageUrl={"/ml-2-2.png"}
+                      />
+                      <SectionCard
+                        name={"Decision Trees and Random Forests"}
+                        imageUrl={"/ml-2-3.png"}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 py-4">
+                    <ChapterDivider
+                      chapter={3}
+                      title={"Unsupervised Learning"}
+                      imageUrl={""}
+                    />
+                    <div className="flex max-w-full flex-row gap-2 overflow-x-auto">
+                      <SectionCard
+                        name={"Clustering with K-Means"}
+                        imageUrl={"/ml-3-1.png"}
+                      />
+                      <SectionCard
+                        name={"Dimensionality Reduction with PCA"}
+                        imageUrl={"/ml-3-2.png"}
+                      />
+                      <SectionCard
+                        name={"Neural Networks and Deep Learning Basics"}
+                        imageUrl={"/ml-3-3.png"}
+                      />
                     </div>
                   </div>
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <div className="flex h-[50px] w-full items-center justify-between bg-card p-2">
+          <div className="flex h-[50px] w-full items-center justify-between p-2 ">
             <CarouselPrevious />
-            <div className="flex flex-row justify-center">
-              <Dot className="h-5 w-5 text-primary/30 transition-all" />
-              <Dot className="h-5 w-5 text-primary transition-all" />
-              <Dot className="h-5 w-5 text-primary/30 transition-all" />
-            </div>
             <CarouselNext />
           </div>
         </Carousel>
