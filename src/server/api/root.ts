2f401c2aeb10df73db419de7b05926060bb53aa5
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-29 14:10:50 -0500

    Add quiz router to appRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,12 +3,14 @@ import { userRouter } from "./routers/users";
 import { courseRouter } from "./routers/courses";
 import { aiRouter } from "./routers/ai";
 import { notesRouter } from "./routers/notes";
+import { quizRouter } from "./routers/quiz";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   course: courseRouter,
   ai: aiRouter,
   notes: notesRouter,
+  quiz: quizRouter,
 });
 
 export type AppRouter = typeof appRouter;
