4bc9f8fb34f9cda9add6395bb3af9fb63cb8d2e4
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-01 15:05:22 -0500

    preloadimages component

---
+++ src/app/notes/components/notesmenu/preloadimages.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import useNotesMenu from "./useNotesMenu";
+
+export default function PreloadImages({
+  preloadImages,
+  setPreloadImagesList,
+  notes,
+}: Pick<
+  ReturnType<typeof useNotesMenu>,
+  "preloadImages" | "setPreloadImagesList" | "notes"
+>) {
+  if (!preloadImages) {
+    return null;
+  }
+
+  return (
+    <>
+      {notes.map((note) => {
+        return (
+          <div key={note.id} className="h-0 w-0 opacity-0">
+            <Image
+              src={note.imageUrl ?? "/generating1.gif"}
+              alt="Preload image"
+              width={300}
+              height={300}
+              className=""
+              onLoad={() => {
+                setPreloadImagesList((prevState) => [
+                  ...prevState,
+                  note.imageUrl!,
+                ]);
+              }}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+}
