da762bbe11c2937b81c634bc6fd86da3bb95fef3
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-05 16:31:57 -0500

    more work on quiz page play component

--- src/app/quiz/[noteId]/play/page.tsx
+++ src/app/quiz/[noteId]/play/page.tsx
@@ -6,6 +6,13 @@ import { useEffect, useState } from "react";
 import Quiz from "./questionComponents/quiz/quiz";
 import Sort from "./questionComponents/sort/sort";
 import Understanding from "./questionComponents/understanding/understanding";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Page({
   params: { noteId },
@@ -32,25 +39,46 @@ export default function Page({
   return (
     <>
       <QuizHeading q={q} noteId={noteId} />
-      <div className="flex h-screen w-screen items-center justify-center pt-20">
-        <div className="flex flex-col items-center justify-center gap-2">
-          {questions?.questions?.map((question) => {
-            return (
-              <div>
-                {question.type === "QUIZ" &&
-                question.quizzes ? null : question.type === "SORTING" &&
-                  question.sorting ? null : question.type === "UNDERSTANDING" &&
-                  question.understanding ? (
-                  <Understanding
-                    question={question.understanding?.questionMarkdown}
-                    completed={false}
-                    explanation={question.understanding.explanationMarkdown}
-                  />
-                ) : null}
-              </div>
-            );
-          })}
-        </div>
+      <div className=" flex h-screen w-screen items-center justify-center overflow-x-hidden pt-12">
+        <Carousel className="flex w-full flex-col items-center justify-center">
+          <CarouselContent>
+            {questions?.questions?.map((question) => {
+              return (
+                <CarouselItem className="flex items-center justify-center">
+                  <div className="flex max-w-[800px] flex-col px-8">
+                    {question.type === "QUIZ" && question.quizzes ? (
+                      <Quiz
+                        key={question.quizzes.id}
+                        completed={false}
+                        explanation={question.quizzes.explanationMarkdown}
+                        //@ts-ignore
+                        options={question.quizzes.options}
+                        content={question.quizzes.questionMarkdown}
+                        answer={question.quizzes.correctOption}
+                      />
+                    ) : question.type === "SORTING" && question.sorting ? (
+                      <Sort
+                        completed={false}
+                        explanation={question.sorting.explanationMarkdown}
+                        options={question.sorting.options}
+                        question={question.sorting.questionMarkdown}
+                        key={question.sorting.id}
+                      />
+                    ) : question.type === "UNDERSTANDING" &&
+                      question.understanding ? (
+                      <Understanding
+                        key={question.understanding.id}
+                        question={question.understanding?.questionMarkdown}
+                        completed={false}
+                        explanation={question.understanding.explanationMarkdown}
+                      />
+                    ) : null}
+                  </div>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+        </Carousel>
       </div>
     </>
   );
