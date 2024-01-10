833f978501f63d98f8d885e7f286345547611d49
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-10 16:53:44 -0500

    right and wrong colors for quiz

--- src/app/quiz/[noteId]/play/questionComponents/quiz/quiz.tsx
+++ src/app/quiz/[noteId]/play/questionComponents/quiz/quiz.tsx
@@ -4,7 +4,6 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect, SetStateAction, Dispatch } from "react";
 import useSound from "use-sound";
-import { toast } from "sonner";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
@@ -30,10 +29,6 @@ export default function Quiz({
   completed?: boolean;
   api: CarouselApi | undefined;
 }) {
-  const correct = () => toast.success("correct!");
-
-  const incorrect = () => toast.error("incorrect!");
-
   const [guessed, setGuessed] = useState<number[]>([]);
 
   const [isFlipped, setIsFlipped] = useState(false);
@@ -42,8 +37,11 @@ export default function Quiz({
   const [correctSound] = useSound("/correct.mp3", { volume: 0.5 });
   const [incorrectSound] = useSound("/incorrect.mp3", { volume: 0.5 });
   const [flipSound] = useSound("/flip.mp3", { volume: 0.5 });
+  const [incorrectVal, setIncorrectVal] = useState(false);
+  const [correctVal, setCorrectVal] = useState(false);
 
   const [side, setSide] = useState<"QUESTION" | "ANSWER">("QUESTION");
+  const [continueSound] = useSound("/click.mp3", { volume: 0.5 });
 
   useEffect(() => {
     if (completed) {
@@ -76,17 +74,59 @@ export default function Quiz({
     });
     if (guess === answer) {
       correctSound();
-      correct();
       setCompleted((prev) => ({ ...prev, [index]: true }));
+      setCorrectVal(true);
     } else {
       incorrectSound();
-      incorrect();
+      setIncorrectVal(true);
     }
   }
 
   return (
     <div className="p-4">
-      <div className="card-container ">
+      <div className="card-container relative">
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "640px",
+            backgroundImage: `radial-gradient(at 27% 37%, hsla(0, 50%, 50%, 1) 0px, transparent 50%),
+                      radial-gradient(at 97% 21%, hsla(5, 50%, 60%, 1) 0px, transparent 50%),
+                      radial-gradient(at 52% 99%, hsla(10, 50%, 55%, 1) 0px, transparent 50%),
+                      radial-gradient(at 10% 29%, hsla(15, 50%, 65%, 1) 0px, transparent 50%),
+                      radial-gradient(at 97% 96%, hsla(20, 50%, 70%, 1) 0px, transparent 50%),
+                      radial-gradient(at 33% 50%, hsla(25, 50%, 75%, 1) 0px, transparent 50%),
+                      radial-gradient(at 79% 53%, hsla(30, 50%, 80%, 1) 0px, transparent 50%)`,
+            position: "absolute",
+            height: "100%",
+            filter: "blur(100px) saturate(150%)",
+            top: "80px",
+            opacity: 0.15,
+          }}
+          className={`absolute bottom-0 left-0 right-0 top-0 transition-all ${
+            incorrectVal && !correctVal ? "" : "hidden opacity-0"
+          }`}
+        ></div>
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "640px",
+            backgroundImage: `radial-gradient(at 27% 37%, hsla(90, 50%, 50%, 1) 0px, transparent 50%),
+                      radial-gradient(at 97% 21%, hsla(100, 50%, 60%, 1) 0px, transparent 50%),
+                      radial-gradient(at 52% 99%, hsla(110, 50%, 55%, 1) 0px, transparent 50%),
+                      radial-gradient(at 10% 29%, hsla(120, 50%, 65%, 1) 0px, transparent 50%),
+                      radial-gradient(at 97% 96%, hsla(130, 50%, 70%, 1) 0px, transparent 50%),
+                      radial-gradient(at 33% 50%, hsla(140, 50%, 75%, 1) 0px, transparent 50%),
+                      radial-gradient(at 79% 53%, hsla(150, 50%, 80%, 1) 0px, transparent 50%)`,
+            position: "absolute",
+            height: "100%",
+            filter: "blur(100px) saturate(150%)",
+            top: "80px",
+            opacity: 0.15,
+          }}
+          className={`absolute bottom-0 left-0 right-0 top-0 transition-all ${
+            correctVal ? "" : "hidden opacity-0"
+          }`}
+        ></div>
         <div
           className={`card rounded-lg border ${isFlipped ? "is-flipped" : ""}`}
         >
@@ -137,7 +177,12 @@ export default function Quiz({
                       Check
                     </Button>
                   ) : (
-                    <Button onClick={() => api?.scrollTo(index + 1)}>
+                    <Button
+                      onClick={() => {
+                        continueSound();
+                        api?.scrollTo(index + 1);
+                      }}
+                    >
                       Continue
                     </Button>
                   )}
