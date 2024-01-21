f0d6986a9fb127fcb0e4588a48c6cc3fee761e31
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-21 08:05:43 -0500

    dashboard moves to proper new form

--- src/app/dashboard/components/dashboard.tsx
+++ src/app/dashboard/components/dashboard.tsx
@@ -6,55 +6,57 @@ import LearningPath from "./learningpath/learningpath";
 
 export default function Dashboard() {
   return (
-    <div>
-      <div className="hidden 2xl:block">
-        <div className="flex w-full flex-row gap-4 rounded-lg">
-          <div className="w-[70%]">
-            <div className="flex flex-col gap-4">
-              <div className="flex w-full gap-4">
-                <div className="w-[60%]">
-                  <LatestActivity />
+    <div className="relative flex w-full flex-col gap-8 py-4">
+      <div className="mx-auto w-full">
+        <div className="hidden 2xl:block">
+          <div className="relative flex w-full flex-row gap-4 rounded-lg">
+            <div className="flex w-[70%] flex-grow flex-col">
+              <div className="flex h-[775px] flex-col justify-between gap-4">
+                <div className="flex h-full w-full gap-4">
+                  <div className="w-[55%]">
+                    <LatestActivity />
+                  </div>
+                  <div className="w-[45%]">
+                    <NotesMenu />
+                  </div>
                 </div>
-                <div className="w-[40%]">
-                  <NotesMenu />
+                <div className="rounded-lg bg-card/70 dark:bg-card/80">
+                  <Streak />
                 </div>
               </div>
-              <div className="rounded-lg bg-card/70 dark:bg-card/80">
-                <Streak />
-              </div>
             </div>
-          </div>
-          <div className="w-[30%]">
-            <LearningPath />
-          </div>
-        </div>
-      </div>
-      <div className="hidden md:block 2xl:hidden">
-        <div className="flex flex-col gap-4 rounded-lg">
-          <div className="flex w-full gap-4">
-            <div className="w-[55%]">
-              <LatestActivity />
-            </div>
-            <div className="w-[45%]">
-              <NotesMenu />
-            </div>
-          </div>
-          <div>
-            <div className="w-full">
+            <div className="flex w-[30%] flex-grow flex-col">
               <LearningPath />
             </div>
           </div>
-          <div className="rounded-lg bg-card/70 dark:bg-card/80">
-            <Streak />
+        </div>
+        <div className="hidden md:block 2xl:hidden">
+          <div className="flex flex-col gap-4 rounded-lg">
+            <div className="flex w-full gap-4">
+              <div className="w-[55%]">
+                <LatestActivity />
+              </div>
+              <div className="w-[45%]">
+                <NotesMenu />
+              </div>
+            </div>
+            <div>
+              <div className="w-full">
+                <LearningPath />
+              </div>
+            </div>
+            <div className="rounded-lg bg-card/70 dark:bg-card/80">
+              <Streak />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="block md:hidden">
-        <div className="flex flex-col gap-4 rounded-lg">
-          <LatestActivity />
-          <NotesMenu />
-          <LearningPath />
-          <Streak />
+        <div className="block md:hidden">
+          <div className="flex flex-col gap-4 rounded-lg">
+            <LatestActivity />
+            <NotesMenu />
+            <LearningPath />
+            <Streak />
+          </div>
         </div>
       </div>
     </div>
