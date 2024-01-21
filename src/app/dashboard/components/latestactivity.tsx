a23156746b9cbf444a273aae7afa541a95c8362b
Noah Solomon <noahsolomon2003@gmail.com>
2024-01-21 08:08:50 -0500

    latest activity and recommended lesson formatted to new form

--- src/app/dashboard/components/latestactivity.tsx
+++ src/app/dashboard/components/latestactivity.tsx
@@ -22,12 +22,11 @@ export default function LatestActivity() {
 
   //@ts-ignore
   if (!latestQuery.isLoading && latest && !latest.latest) {
-    console.log(true);
     return <RecommendedLesson />;
   }
 
   return (
-    <div className="relative flex w-full flex-col gap-4 rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 md:w-auto">
+    <div className="relative flex h-full w-full flex-col justify-between rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 md:w-auto ">
       <Badge
         className="absolute -right-6 -top-2 z-30 p-1 text-sm"
         variant={"arts"}
@@ -37,54 +36,55 @@ export default function LatestActivity() {
       {process.env.NEXT_PUBLIC_ENV === "PROD" ? (
         <div className="absolute bottom-0 left-0 right-0 top-0 z-20 flex items-center justify-center rounded-lg "></div>
       ) : null}
+      <div className="flex flex-col gap-4">
+        <div className="w-full">
+          {latestQuery.isLoading && (
+            <AspectRatio ratio={7 / 4}>
+              <Skeleton className={`h-full w-full rounded-lg`}></Skeleton>
+            </AspectRatio>
+          )}
+          <Image
+            src={
+              latestQuery.isLoading || !latest
+                ? "https://images.codefoli.com/systems-design-1-1.png"
+                : latest.latest.courseChapterSections?.imageUrl ??
+                  "https://images.codefoli.com/systems-design-1-1.png"
+            }
+            priority={true}
+            alt="lesson"
+            width={700}
+            height={200}
+            className={`max-h-[275px] max-w-full rounded-lg ${
+              latestQuery.isLoading || !latest?.latest
+                ? "h-0 w-0 max-w-0 opacity-0"
+                : "opacity-100"
+            }`}
+          />
+        </div>
 
-      <div className="w-full">
-        {latestQuery.isLoading && (
-          <AspectRatio ratio={7 / 4}>
-            <Skeleton className={`h-full w-full rounded-lg`}></Skeleton>
-          </AspectRatio>
-        )}
-        <Image
-          src={
-            latestQuery.isLoading || !latest
-              ? "https://images.codefoli.com/systems-design-1-1.png"
-              : latest.latest.courseChapterSections?.imageUrl ??
-                "https://images.codefoli.com/systems-design-1-1.png"
-          }
-          priority={true}
-          alt="lesson"
-          width={700}
-          height={200}
-          className={`max-w-full rounded-lg ${
-            latestQuery.isLoading || !latest?.latest
-              ? "h-0 w-0 max-w-0 opacity-0"
-              : "opacity-100"
-          }`}
-        />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        {latestQuery.isLoading || !latest ? (
-          <>
-            <Skeleton className="h-12 w-24 rounded-lg"></Skeleton>
-            <Skeleton className="h-6 w-[75%] rounded-lg"></Skeleton>
-            <Skeleton className="h-6 w-12 rounded-lg"></Skeleton>
-          </>
-        ) : (
-          <>
-            <h3>
-              {latest.latest.courseChapterSections?.course.name ??
-                "Systems Design"}
-            </h3>
-            <p className="hidden max-w-[30ch] text-base md:block">
-              {latest.latest.courseChapterSections?.name ?? "Lesson"}
-            </p>
-            <p className="text-sm opacity-80">
-              Lesson {latest.latest.courseChapterSections?.lessonNumber} of{" "}
-              {latest.latest.courseChapterSections?.course.lessons}
-            </p>
-          </>
-        )}
+        <div className="flex flex-col gap-2">
+          {latestQuery.isLoading || !latest ? (
+            <>
+              <Skeleton className="h-12 w-24 rounded-lg"></Skeleton>
+              <Skeleton className="h-6 w-[75%] rounded-lg"></Skeleton>
+              <Skeleton className="h-6 w-12 rounded-lg"></Skeleton>
+            </>
+          ) : (
+            <>
+              <h3>
+                {latest.latest.courseChapterSections?.course.name ??
+                  "Systems Design"}
+              </h3>
+              <p className="hidden max-w-[30ch] text-base md:block">
+                {latest.latest.courseChapterSections?.name ?? "Lesson"}
+              </p>
+              <p className="text-sm opacity-80">
+                Lesson {latest.latest.courseChapterSections?.lessonNumber} of{" "}
+                {latest.latest.courseChapterSections?.course.lessons}
+              </p>
+            </>
+          )}
+        </div>
       </div>
       {latestQuery.isLoading || !latest ? (
         <>
@@ -92,7 +92,7 @@ export default function LatestActivity() {
           <Skeleton className="h-8 w-full rounded-lg"></Skeleton>
         </>
       ) : (
-        <>
+        <div className="flex w-full flex-col gap-2">
           <Progress
             className="h-4 border-2 border-border"
             indicatorClassName="bg-blue/80 rounded-r-lg"
@@ -108,9 +108,8 @@ export default function LatestActivity() {
             className={cn(buttonVariants(), `transition-all`)}
           >
             Continue
-            <ArrowRightCircle className=" h-5 w-5 pl-1" />
           </Link>
-        </>
+        </div>
       )}
     </div>
   );
--- src/app/dashboard/components/recommendedlesson.tsx
+++ src/app/dashboard/components/recommendedlesson.tsx
@@ -4,7 +4,6 @@ import { Badge } from "@/components/ui/badge";
 import { buttonVariants } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { cn } from "@/lib/utils";
-import { ArrowRightCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import useSound from "use-sound";
@@ -13,7 +12,7 @@ export default function RecommendedLesson() {
   const [click] = useSound("/click.mp3", { volume: 0.5 });
 
   return (
-    <div className="relative flex w-full flex-col gap-4 rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 md:w-auto">
+    <div className="relative flex h-full w-full flex-col justify-between rounded-lg border bg-card/70 p-4 shadow-md dark:bg-card/80 md:w-auto ">
       <Badge
         className="absolute -right-6 -top-2 z-30 p-1 text-sm"
         variant={"arts"}
@@ -23,36 +22,39 @@ export default function RecommendedLesson() {
       {process.env.NEXT_PUBLIC_ENV === "PROD" ? (
         <div className="absolute bottom-0 left-0 right-0 top-0 z-20 flex items-center justify-center rounded-lg"></div>
       ) : null}
-
-      <div className="w-full">
-        <Image
-          src={"https://images.codefoli.com/systems-design-1-1.png"}
-          alt="lesson"
-          width={700}
-          className="max-w-full rounded-lg"
-          height={200}
-          priority={true}
+      <div className="flex flex-col gap-4">
+        <div className="w-full">
+          <Image
+            src={"https://images.codefoli.com/systems-design-1-1.png"}
+            alt="lesson"
+            width={700}
+            className="max-h-[275px] max-w-full rounded-lg"
+            height={200}
+            priority={true}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3>Systems Design</h3>
+          <p className="max-w-[30ch] text-base">
+            Introduction to Systems Design: Concepts and Importance
+          </p>
+          <p className="text-sm opacity-80">Lesson 1 of 40</p>
+        </div>
+      </div>
+      <div className="flex w-full flex-col gap-2">
+        <Progress
+          className="h-4 border-2 border-border"
+          indicatorClassName="bg-blue rounded-r-lg"
+          value={0}
         />
+        <Link
+          onClick={() => click()}
+          href={`/lesson/systems-design/1/25?l=1`}
+          className={cn(buttonVariants(), `transition-all`)}
+        >
+          Start lesson
+        </Link>
       </div>
-      <div className="flex flex-col gap-2">
-        <h3>Systems Design</h3>
-        <p className="max-w-[30ch] text-base">
-          Introduction to Systems Design: Concepts and Importance
-        </p>
-        <p className="text-sm opacity-80">Lesson 1 of 40</p>
-      </div>
-      <Progress
-        className="h-4 border-2 border-border"
-        indicatorClassName="bg-blue rounded-r-lg"
-        value={0}
-      />
-      <Link
-        onClick={() => click()}
-        href={`/lesson/systems-design/1/25?l=1`}
-        className={cn(buttonVariants(), `transition-all`)}
-      >
-        Start lesson <ArrowRightCircle className=" h-5 w-5 pl-1" />
-      </Link>
     </div>
   );
 }
