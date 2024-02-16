6c850b1c3fe17b4e3b8a6f0d400b48dcce6b6d69
Noah Solomon <noahsolomon2003@gmail.com>
2024-02-16 14:17:58 -0500

    Add badge to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,26 @@
+import { Badge } from "@/components/ui/badge";
 import DashboardNew from "./dashboardnew";
 
 const Page = async () => {
   return (
     <>
-      <main className="mx-4 mt-24 flex h-[60vh] items-center justify-center transition-all md:mx-20 xl:mx-80">
+      <main className="mx-4 flex h-[60vh] flex-col items-center justify-center pt-24 transition-all md:mx-20 xl:mx-80">
+        <div className="flex flex-col items-center justify-center gap-8 pb-8">
+          <div className=" flex flex-col items-center gap-2">
+            <Badge className="text-lg md:hidden" variant={"math"}>
+              BETA
+            </Badge>
+            <h1 className="relative max-w-[10ch] text-center text-5xl font-bold text-primary lg:text-6xl">
+              SMART.WTF
+              <Badge
+                className="absolute -top-8 hidden text-lg md:-right-12 md:block"
+                variant={"math"}
+              >
+                BETA
+              </Badge>
+            </h1>
+          </div>
+        </div>
         <DashboardNew />
       </main>
     </>
