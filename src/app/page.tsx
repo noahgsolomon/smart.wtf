c5f8a54ee01fe7a4f596744938af79ad99a3c9e5
Noah Solomon <noahsolomon2003@gmail.com>
2024-02-16 17:46:16 -0500

    new hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import DemoLatestActivity from "@/components/demo/demolatestactivity";
 import DemoStreak from "@/components/demo/demostreak";
 import DemoLearningPath from "@/components/demo/demolearningpath";
 import { Badge } from "@/components/ui/badge";
+import DashboardNew from "./dashboard/dashboardnew";
 
 export default function Home() {
   const user = auth();
@@ -14,79 +15,24 @@ export default function Home() {
   }
 
   return (
-    <main className="relative mt-6 flex flex-col items-center justify-center gap-4">
-      <div className="mt-[100px] flex w-[90%] flex-col items-center justify-center bg-opacity-60 text-4xl lg:w-[80%] xl:w-[75%]">
-        <div className="flex flex-col items-center justify-center gap-8 pb-8">
-          <div className=" flex flex-col items-center gap-2">
-            <Badge className="text-lg md:hidden" variant={"math"}>
+    <main className="mx-4 flex h-[60vh] flex-col items-center justify-center pt-24 transition-all md:mx-20 xl:mx-80">
+      <div className="flex flex-col items-center justify-center gap-8 pb-8">
+        <div className=" flex flex-col items-center gap-2">
+          <Badge className="text-lg md:hidden" variant={"math"}>
+            BETA
+          </Badge>
+          <h1 className="relative max-w-[10ch] text-center text-5xl font-bold text-primary lg:text-6xl">
+            SMART.WTF
+            <Badge
+              className="absolute -top-8 hidden text-lg md:-right-12 md:block"
+              variant={"math"}
+            >
               BETA
             </Badge>
-            <h1 className="relative max-w-[10ch] text-center text-5xl font-bold lg:text-6xl">
-              SMART.WTF
-              <Badge
-                className="absolute -top-8 hidden text-lg md:-right-12 md:block"
-                variant={"math"}
-              >
-                BETA
-              </Badge>
-            </h1>
-          </div>
-        </div>
-        <div className="relative flex w-full flex-col gap-8 py-4">
-          <div className="mx-auto w-full">
-            <div className="hidden 2xl:block">
-              <div className="relative flex w-full flex-row gap-4 rounded-lg">
-                <div className="flex w-[70%] flex-grow flex-col">
-                  <div className="flex h-[775px] flex-col justify-between gap-4">
-                    <div className="flex h-full w-full gap-4">
-                      <div className="w-[60%]">
-                        <DemoLatestActivity />
-                      </div>
-                      <div className="w-[40%]">
-                        <DemoNotesMenu />
-                      </div>
-                    </div>
-                    <div className="rounded-lg bg-card/70 dark:bg-card/80">
-                      <DemoStreak />
-                    </div>
-                  </div>
-                </div>
-                <div className="flex w-[30%] flex-grow flex-col">
-                  <DemoLearningPath />
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:block 2xl:hidden">
-              <div className="flex flex-col gap-4 rounded-lg">
-                <div className="flex w-full gap-4">
-                  <div className="w-[55%]">
-                    <DemoLatestActivity />
-                  </div>
-                  <div className="w-[45%]">
-                    <DemoNotesMenu />
-                  </div>
-                </div>
-                <div>
-                  <div className="w-full">
-                    <DemoLearningPath />
-                  </div>
-                </div>
-                <div className="rounded-lg bg-card/70 dark:bg-card/80">
-                  <DemoStreak />
-                </div>
-              </div>
-            </div>
-            <div className="block md:hidden">
-              <div className="flex flex-col gap-4 rounded-lg">
-                <DemoLatestActivity />
-                <DemoNotesMenu />
-                <DemoLearningPath />
-                <DemoStreak />
-              </div>
-            </div>
-          </div>
+          </h1>
         </div>
       </div>
+      <DashboardNew />
     </main>
   );
 }
