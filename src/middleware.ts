c271874938fd31c52cc035c258dd771ce981335b
Noah Solomon <noahsolomon2003@gmail.com>
2024-02-17 11:27:09 -0500

    new middleware public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,17 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
+    "/notes/:noteId/public",
     "/notes/:noteId",
     "/api/webhooks/stripe",
     "/api/trpc/quiz.isQuizAvailable",
     "/api/trpc/notes.getUserNotesMeta",
     "/api/trpc/notes.findRandomNotes",
     "/api/trpc/notes.getNote",
+    "/api/trpc/notes.getNote,quiz.isQuizAvailable",
+    "/api/trpc/notes.validateTopic",
+    "/api/trpc/notes.validateTopic,notes.findRandomNotes",
+    "/api/trpc/notes.findRandomNotes,notes.validateTopic",
   ],
   ignoredRoutes: ["/api/webhooks/stripe"],
 });
