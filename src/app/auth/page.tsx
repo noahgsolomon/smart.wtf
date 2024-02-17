713896bcf394de9bdd0b1b93a200c167518cef51
Noah Solomon <noahsolomon2003@gmail.com>
2024-02-17 11:27:22 -0500

    / is now main route

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,9 +2,34 @@ import { api } from "@/trpc/server";
 import { Loader2 } from "lucide-react";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: {
+    noteTopic?: string;
+    noteAgent?: string;
+    noteCategory?: string;
+    noteNextTopic?: string;
+  };
+}) => {
   await api.user.exists.mutate();
-  redirect("/dashboard");
+  console.log(searchParams);
+  redirect(
+    `/${
+      searchParams.noteTopic &&
+      searchParams.noteAgent &&
+      searchParams.noteCategory &&
+      searchParams.noteNextTopic
+        ? `?noteTopic=${encodeURIComponent(
+            searchParams.noteTopic,
+          )}&noteAgent=${encodeURIComponent(
+            searchParams.noteAgent,
+          )}&noteCategory=${encodeURIComponent(
+            searchParams.noteCategory,
+          )}&noteNextTopic=${encodeURIComponent(searchParams.noteNextTopic)}`
+        : ""
+    }`,
+  );
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
